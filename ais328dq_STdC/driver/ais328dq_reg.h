9a2b0f389e4cad4eee770bb205e753785c4463d5
albezanc <alberto.zancanato@st.com>
2018-05-04 15:02:11 +0200

    ais328dq (id20180504): MISRA Compliance extension
    Functions:
     - Add     _from_fs2_to_mg
     - Add     _from_fs4_to_mg
     - Add     _from_fs8_to_mg
    
    Comment:
     - Modify field "author" from comment (.c/.h)
     - Remove field "date" from comment (.c/.h)
    
    MISRA Compliance:
      MISRAC2012-Rule-19.2 status -> union can be convenient for user (.h)
      MISRAC2012-Dir-4.9 status -> macro maintained for back compatibility (.h)

--- ais328dq_STdC/driver/ais328dq_reg.h
+++ ais328dq_STdC/driver/ais328dq_reg.h
@@ -1,42 +1,43 @@
 /*
  ******************************************************************************
  * @file    ais328dq_reg.h
- * @author  MEMS Software Solution Team
- * @date    20-December-2017
+ * @author  Sensors Software Solution Team
  * @brief   This file contains all the functions prototypes for the
  *          ais328dq_reg.c driver.
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; COPYRIGHT(c) 2017 STMicroelectronics</center></h2>
+ * <h2><center>&copy; COPYRIGHT(c) 2018 STMicroelectronics</center></h2>
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *   1. Redistributions of source code must retain the above copyright notice,
  *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright notice,
- *      this list of conditions and the following disclaimer in the documentation
- *      and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its contributors
- *      may be used to endorse or promote products derived from this software
- *      without specific prior written permission.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of STMicroelectronics nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  *
  */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __AIS328DQ_DRIVER__H
-#define __AIS328DQ_DRIVER__H
+#ifndef AIS328DQ_REGS_H
+#define AIS328DQ_REGS_H
 
 #ifdef __cplusplus
   extern "C" {
@@ -45,59 +46,89 @@
 /* Includes ------------------------------------------------------------------*/
 #include <stdint.h>
 
-/** @addtogroup ais328dq
- * @{
- */
-
-#ifndef __MEMS_SHARED__TYPES
-#define __MEMS_SHARED__TYPES
-
-/** @defgroup ST_MEMS_common_types
+/** @addtogroup AIS328DQ
   * @{
+  *
+  */
+
+/** @defgroup AIS328DQ_sensors_common_types
+  * @{
+  *
+  */
+
+#ifndef MEMS_SHARED_TYPES
+#define MEMS_SHARED_TYPES
+/**
+  * Float typedef definition for compliance with:
+  * MISRA-C 2012 [Dir 4.6a] -> " Basic type used without typedef "
+  *
+  */
+typedef float float32_t;
+
+/**
+  * @defgroup axisXbitXX_t
+  * @brief    These unions are useful to represent different sensors data type.
+  *           These unions are not need by the driver.
+  *
+  *           REMOVING the unions you are compliant with:
+  *           MISRA-C 2012 [Rule 19.2] -> " Union are not allowed "
+  *
+  * @{
+  *
   */
 
 typedef union{
-	int16_t i16bit[3];
-	uint8_t u8bit[6];
+  int16_t i16bit[3];
+  uint8_t u8bit[6];
 } axis3bit16_t;
 
 typedef union{
-	int16_t i16bit;
-	uint8_t u8bit[2];
+  int16_t i16bit;
+  uint8_t u8bit[2];
 } axis1bit16_t;
 
 typedef union{
-	int32_t i32bit[3];
-	uint8_t u8bit[12];
+  int32_t i32bit[3];
+  uint8_t u8bit[12];
 } axis3bit32_t;
 
 typedef union{
-	int32_t i32bit;
-	uint8_t u8bit[4];
+  int32_t i32bit;
+  uint8_t u8bit[4];
 } axis1bit32_t;
-
-typedef struct {
-   uint8_t bit0       : 1;
-   uint8_t bit1       : 1;
-   uint8_t bit2       : 1;
-   uint8_t bit3       : 1;
-   uint8_t bit4       : 1;
-   uint8_t bit5       : 1;
-   uint8_t bit6       : 1;
-   uint8_t bit7       : 1;
-} bitwise_t;
-
-#define PROPERTY_DISABLE                (0)
-#define PROPERTY_ENABLE                 (1)
-
-#endif /*__MEMS_SHARED__TYPES*/
 
 /**
   * @}
+  *
   */
 
-/** @defgroup ais328dq_interface
+typedef struct{
+  uint8_t bit0       : 1;
+  uint8_t bit1       : 1;
+  uint8_t bit2       : 1;
+  uint8_t bit3       : 1;
+  uint8_t bit4       : 1;
+  uint8_t bit5       : 1;
+  uint8_t bit6       : 1;
+  uint8_t bit7       : 1;
+} bitwise_t;
+
+#define PROPERTY_DISABLE                (0U)
+#define PROPERTY_ENABLE                 (1U)
+
+#endif /* MEMS_SHARED_TYPES */
+
+/**
+  * @}
+  *
+  */
+
+/** @addtogroup  AIS328DQ_Interfaces_Functions
+  * @brief       This section provide a set of functions used to read and
+  *              write a generic register of the device.
+  *              MANDATORY: return 0 -> no Error.
   * @{
+  *
   */
 
 typedef int32_t (*ais328dq_write_ptr)(void *, uint8_t, uint8_t*, uint16_t);
@@ -113,26 +144,40 @@ typedef struct {
 
 /**
   * @}
+  *
   */
 
 
-/** @defgroup ais328dq_Infos
+/** @defgroup AIS328DQ_Infos
   * @{
+  *
   */
-  /** I2C Device Address 8 bit format  if SA0=0 -> 0x31 if SA0=1 -> 0x33 **/
-#define AIS328DQ_I2C_ADD_L   0x31
-#define AIS328DQ_I2C_ADD_H   0x33
+
+/** I2C Device Address 8 bit format  if SA0=0 -> 0x31 if SA0=1 -> 0x33 **/
+#define AIS328DQ_I2C_ADD_L     0x31
+#define AIS328DQ_I2C_ADD_H     0x33
 
 /** Device Identification (Who am I) **/
 #define AIS328DQ_ID            0x32
 
 /**
   * @}
+  *
   */
 
 /**
-  * @defgroup ais328dq_Sensitivity
+  * @addtogroup  AIS328DQ_Sensitivity
+  * @brief       These macro are maintained for back compatibility.
+  *              in order to convert data into engineering units please
+  *              use functions:
+  *                -> _from_fs2_to_mg(int16_t lsb);
+  *                -> _from_fs4_to_mg(int16_t lsb);
+  *                -> _from_fs8_to_mg(int16_t lsb);
+  *
+  *              REMOVING the MACRO you are compliant with:
+  *              MISRA-C 2012 [Dir 4.9] -> " avoid function-like macros "
   * @{
+  *
   */
 
 #define AIS328DQ_FROM_FS_2g_TO_mg(lsb)    (float)( (lsb >> 4 ) * 0.98f )
@@ -141,10 +186,11 @@ typedef struct {
 
 /**
   * @}
+  *
   */
 
-#define AIS328DQ_WHO_AM_I                  0x0F
-#define AIS328DQ_CTRL_REG1                 0x20
+#define AIS328DQ_WHO_AM_I                  0x0FU
+#define AIS328DQ_CTRL_REG1                 0x20U
 typedef struct {
   uint8_t xen                      : 1;
   uint8_t yen                      : 1;
@@ -153,7 +199,7 @@ typedef struct {
   uint8_t pm                       : 3;
 } ais328dq_ctrl_reg1_t;
 
-#define AIS328DQ_CTRL_REG2                 0x21
+#define AIS328DQ_CTRL_REG2                 0x21U
 typedef struct {
   uint8_t hpcf                     : 2;
   uint8_t hpen                     : 2;
@@ -162,7 +208,7 @@ typedef struct {
   uint8_t boot                     : 1;
 } ais328dq_ctrl_reg2_t;
 
-#define AIS328DQ_CTRL_REG3                 0x22
+#define AIS328DQ_CTRL_REG3                 0x22U
 typedef struct {
   uint8_t i1_cfg                   : 2;
   uint8_t lir1                     : 1;
@@ -172,7 +218,7 @@ typedef struct {
   uint8_t ihl                      : 1;
 } ais328dq_ctrl_reg3_t;
 
-#define AIS328DQ_CTRL_REG4                 0x23
+#define AIS328DQ_CTRL_REG4                 0x23U
 typedef struct {
   uint8_t sim                      : 1;
   uint8_t st                       : 3; /* STsign + ST */
@@ -181,15 +227,15 @@ typedef struct {
   uint8_t bdu                      : 1;
 } ais328dq_ctrl_reg4_t;
 
-#define AIS328DQ_CTRL_REG5                 0x24
+#define AIS328DQ_CTRL_REG5                 0x24U
 typedef struct {
   uint8_t turnon                   : 2;
   uint8_t not_used_01              : 6;
 } ais328dq_ctrl_reg5_t;
 
-#define AIS328DQ_HP_FILTER_RESET           0x25
-#define AIS328DQ_REFERENCE                 0x26
-#define AIS328DQ_STATUS_REG                0x27
+#define AIS328DQ_HP_FILTER_RESET           0x25U
+#define AIS328DQ_REFERENCE                 0x26U
+#define AIS328DQ_STATUS_REG                0x27U
 typedef struct {
   uint8_t xda                      : 1;
   uint8_t yda                      : 1;
@@ -201,13 +247,13 @@ typedef struct {
   uint8_t zyxor                    : 1;
 } ais328dq_status_reg_t;
 
-#define AIS328DQ_OUT_X_L                   0x28
-#define AIS328DQ_OUT_X_H                   0x29
-#define AIS328DQ_OUT_Y_L                   0x2A
-#define AIS328DQ_OUT_Y_H                   0x2B
-#define AIS328DQ_OUT_Z_L                   0x2C
-#define AIS328DQ_OUT_Z_H                   0x2D
-#define AIS328DQ_INT1_CFG                  0x30
+#define AIS328DQ_OUT_X_L                   0x28U
+#define AIS328DQ_OUT_X_H                   0x29U
+#define AIS328DQ_OUT_Y_L                   0x2AU
+#define AIS328DQ_OUT_Y_H                   0x2BU
+#define AIS328DQ_OUT_Z_L                   0x2CU
+#define AIS328DQ_OUT_Z_H                   0x2DU
+#define AIS328DQ_INT1_CFG                  0x30U
 typedef struct {
   uint8_t xlie                     : 1;
   uint8_t xhie                     : 1;
@@ -219,7 +265,7 @@ typedef struct {
   uint8_t aoi                      : 1;
 } ais328dq_int1_cfg_t;
 
-#define AIS328DQ_INT1_SRC                  0x31
+#define AIS328DQ_INT1_SRC                  0x31U
 typedef struct {
   uint8_t xl                       : 1;
   uint8_t xh                       : 1;
@@ -231,19 +277,19 @@ typedef struct {
   uint8_t not_used_01              : 1;
 } ais328dq_int1_src_t;
 
-#define AIS328DQ_INT1_THS                  0x32
+#define AIS328DQ_INT1_THS                  0x32U
 typedef struct {
   uint8_t ths                      : 7;
   uint8_t not_used_01              : 1;
 } ais328dq_int1_ths_t;
 
-#define AIS328DQ_INT1_DURATION             0x33
+#define AIS328DQ_INT1_DURATION             0x33U
 typedef struct {
   uint8_t d                        : 7;
   uint8_t not_used_01              : 1;
 } ais328dq_int1_duration_t;
 
-#define AIS328DQ_INT2_CFG                  0x34
+#define AIS328DQ_INT2_CFG                  0x34U
 typedef struct {
   uint8_t xlie                     : 1;
   uint8_t xhie                     : 1;
@@ -255,7 +301,7 @@ typedef struct {
   uint8_t aoi                      : 1;
 } ais328dq_int2_cfg_t;
 
-#define AIS328DQ_INT2_SRC                  0x35
+#define AIS328DQ_INT2_SRC                  0x35U
 typedef struct {
   uint8_t xl                       : 1;
   uint8_t xh                       : 1;
@@ -267,18 +313,30 @@ typedef struct {
   uint8_t not_used_01              : 1;
 } ais328dq_int2_src_t;
 
-#define AIS328DQ_INT2_THS                  0x36
+#define AIS328DQ_INT2_THS                  0x36U
 typedef struct {
   uint8_t ths                      : 7;
   uint8_t not_used_01              : 1;
 } ais328dq_int2_ths_t;
 
-#define AIS328DQ_INT2_DURATION             0x37
+#define AIS328DQ_INT2_DURATION             0x37U
 typedef struct {
   uint8_t d                        : 7;
   uint8_t not_used_01              : 1;
 } ais328dq_int2_duration_t;
 
+/**
+  * @defgroup AIS328DQ_Register_Union
+  * @brief    This union group all the registers that has a bit-field
+  *           description.
+  *           This union is useful but not need by the driver.
+  *
+  *           REMOVING this union you are compliant with:
+  *           MISRA-C 2012 [Rule 19.2] -> " Union are not allowed "
+  *
+  * @{
+  *
+  */
 typedef union{
   ais328dq_ctrl_reg1_t                     ctrl_reg1;
   ais328dq_ctrl_reg2_t                     ctrl_reg2;
@@ -298,10 +356,19 @@ typedef union{
   uint8_t                                   byte;
 } ais328dq_reg_t;
 
+/**
+  * @}
+  *
+  */
+
 int32_t ais328dq_read_reg(ais328dq_ctx_t *ctx, uint8_t reg, uint8_t* data,
                            uint16_t len);
 int32_t ais328dq_write_reg(ais328dq_ctx_t *ctx, uint8_t reg, uint8_t* data,
                             uint16_t len);
+
+extern float32_t ais328dq_from_fs2_to_mg(int16_t lsb);
+extern float32_t ais328dq_from_fs4_to_mg(int16_t lsb);
+extern float32_t ais328dq_from_fs8_to_mg(int16_t lsb);
 
 int32_t ais328dq_axis_x_data_set(ais328dq_ctx_t *ctx, uint8_t val);
 int32_t ais328dq_axis_x_data_get(ais328dq_ctx_t *ctx, uint8_t *val);
@@ -553,9 +620,9 @@ typedef enum {
   AIS328DQ_6D_INT2_MOVEMENT  = 1,
   AIS328DQ_6D_INT2_POSITION  = 3,
 } ais328dq_int2_6d_t;
-int32_t ais328dq_int2_6d_mode_set(ais328dq_ctx_t *ctx, 
+int32_t ais328dq_int2_6d_mode_set(ais328dq_ctx_t *ctx,
                                    ais328dq_int2_6d_t val);
-int32_t ais328dq_int2_6d_mode_get(ais328dq_ctx_t *ctx, 
+int32_t ais328dq_int2_6d_mode_get(ais328dq_ctx_t *ctx,
                                    ais328dq_int2_6d_t *val);
 
 int32_t ais328dq_int2_6d_src_get(ais328dq_ctx_t *ctx,
@@ -565,13 +632,14 @@ int32_t ais328dq_int2_6d_treshold_set(ais328dq_ctx_t *ctx, uint8_t val);
 int32_t ais328dq_int2_6d_treshold_get(ais328dq_ctx_t *ctx, uint8_t *val);
 
 /**
-  * @}
+  *@}
+  *
   */
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /*__AIS328DQ_DRIVER__H */
+#endif /* AIS328DQ_REGS_H */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
