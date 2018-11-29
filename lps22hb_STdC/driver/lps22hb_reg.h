826cb7a3dbb97571eb07dcd828c7c26b26a29e8f
albezanc <alberto.zancanato@st.com>
2018-11-29 16:23:53 +0100

    lps22hb: MISRA Compliance extension Macro: - Remove   _FROM_LSB_TO_hPa(lsb) - Remove   _FROM_LSB_TO_degC(lsb) Functions: - Add      _from_lsb_to_hpa - Add      _from_lsb_to_degc  MISRA Compliance: - MISRAC2012-Rule-19.2 status -> union are useful but can be safely removed (.h)

--- lps22hb_STdC/driver/lps22hb_reg.h
+++ lps22hb_STdC/driver/lps22hb_reg.h
@@ -1,42 +1,42 @@
 /*
  ******************************************************************************
  * @file    lps22hb_reg.h
- * @author  MEMS Software Solution Team
- * @date    15-September-2017
+ * @author  Sensors Software Solution Team
  * @brief   This file contains all the functions prototypes for the
  *          lps22hb_reg.c driver.
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; COPYRIGHT(c) 2017 STMicroelectronics</center></h2>
+ * <h2><center>&copy; COPYRIGHT(c) 2018 STMicroelectronics</center></h2>
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *   1. Redistributions of source code must retain the above copyright notice,
  *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright notice,
- *      this list of conditions and the following disclaimer in the documentation
- *      and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its contributors
- *      may be used to endorse or promote products derived from this software
- *      without specific prior written permission.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of STMicroelectronics nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  *
  */
-
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __LPS22HB_DRIVER__H
-#define __LPS22HB_DRIVER__H
+#ifndef LPS22HB_REGS_H
+#define LPS22HB_REGS_H
 
 #ifdef __cplusplus
   extern "C" {
@@ -59,6 +59,18 @@
 #ifndef MEMS_SHARED_TYPES
 #define MEMS_SHARED_TYPES
 
+/**
+  * @defgroup axisXbitXX_t
+  * @brief    These unions are useful to represent different sensors data type.
+  *           These unions are not need by the driver.
+  *
+  *           REMOVING the unions you are compliant with:
+  *           MISRA-C 2012 [Rule 19.2] -> " Union are not allowed "
+  *
+  * @{
+  *
+  */
+
 typedef union{
   int16_t i16bit[3];
   uint8_t u8bit[6];
@@ -78,6 +90,11 @@ typedef union{
   int32_t i32bit;
   uint8_t u8bit[4];
 } axis1bit32_t;
+
+/**
+  * @}
+  *
+  */
 
 typedef struct{
   uint8_t bit0       : 1;
@@ -100,8 +117,12 @@ typedef struct{
   *
   */
 
-/** @defgroup lps22hb_interface
+/** @addtogroup  LPS22HB_Interfaces_Functions
+  * @brief       This section provide a set of functions used to read and
+  *              write a generic register of the device.
+  *              MANDATORY: return 0 -> no Error.
   * @{
+  *
   */
 
 typedef int32_t (*lps22hb_write_ptr)(void *, uint8_t, uint8_t*, uint16_t);
@@ -117,36 +138,28 @@ typedef struct {
 
 /**
   * @}
+  *
   */
 
 
-/** @defgroup lps22hb_Infos
+/** @defgroup LSM9DS1_Infos
   * @{
+  *
   */
+
   /** I2C Device Address 8 bit format: if SA0=0 -> 0xB9 if SA0=1 -> 0xBB **/
-#define LPS22HB_I2C_ADD_H   0xBB
-#define LPS22HB_I2C_ADD_L   0xB9
+#define LPS22HB_I2C_ADD_H   0xBBU
+#define LPS22HB_I2C_ADD_L   0xB9U
 
 /** Device Identification (Who am I) **/
-#define LPS22HB_ID            0xB1
+#define LPS22HB_ID            0xB1U
 
 /**
   * @}
+  *
   */
 
-/**
-  * @defgroup lps22hb_Sensitivity
-  * @{
-  */
-
-#define LPS22HB_FROM_LSB_TO_hPa(lsb)     (float)( lsb / 4096.0f )
-#define LPS22HB_FROM_LSB_TO_degC(lsb)    (float)( lsb / 100.0f )
-
-/**
-  * @}
-  */
-
-#define LPS22HB_INTERRUPT_CFG  0x0B
+#define LPS22HB_INTERRUPT_CFG  0x0BU
 typedef struct {
   uint8_t pe               : 2; /* ple + phe -> pe */
   uint8_t lir              : 1;
@@ -157,10 +170,10 @@ typedef struct {
   uint8_t autorifp         : 1;
 } lps22hb_interrupt_cfg_t;
 
-#define LPS22HB_THS_P_L        0x0C
-#define LPS22HB_THS_P_H        0x0D
-#define LPS22HB_WHO_AM_I       0x0F
-#define LPS22HB_CTRL_REG1      0x10
+#define LPS22HB_THS_P_L        0x0CU
+#define LPS22HB_THS_P_H        0x0DU
+#define LPS22HB_WHO_AM_I       0x0FU
+#define LPS22HB_CTRL_REG1      0x10U
 typedef struct {
   uint8_t sim              : 1;
   uint8_t bdu              : 1;
@@ -169,7 +182,7 @@ typedef struct {
   uint8_t not_used_01      : 1;
 } lps22hb_ctrl_reg1_t;
 
-#define LPS22HB_CTRL_REG2      0x11
+#define LPS22HB_CTRL_REG2      0x11U
 typedef struct {
   uint8_t one_shot         : 1;
   uint8_t not_used_01      : 1;
@@ -181,7 +194,7 @@ typedef struct {
   uint8_t boot             : 1;
 } lps22hb_ctrl_reg2_t;
 
-#define LPS22HB_CTRL_REG3      0x12
+#define LPS22HB_CTRL_REG3      0x12U
 typedef struct {
   uint8_t int_s            : 2;
   uint8_t drdy             : 1;
@@ -193,25 +206,25 @@ typedef struct {
 } lps22hb_ctrl_reg3_t;
 
 
-#define LPS22HB_FIFO_CTRL      0x14
+#define LPS22HB_FIFO_CTRL      0x14U
 typedef struct {
   uint8_t wtm              : 5;
   uint8_t f_mode           : 3;
 } lps22hb_fifo_ctrl_t;
 
-#define LPS22HB_REF_P_XL       0x15
-#define LPS22HB_REF_P_L        0x16
-#define LPS22HB_REF_P_H        0x17
-#define LPS22HB_RPDS_L         0x18
-#define LPS22HB_RPDS_H         0x19
+#define LPS22HB_REF_P_XL       0x15U
+#define LPS22HB_REF_P_L        0x16U
+#define LPS22HB_REF_P_H        0x17U
+#define LPS22HB_RPDS_L         0x18U
+#define LPS22HB_RPDS_H         0x19U
 
-#define LPS22HB_RES_CONF       0x1A
+#define LPS22HB_RES_CONF       0x1AU
 typedef struct {
   uint8_t lc_en            : 1;
   uint8_t not_used_01      : 7;
 } lps22hb_res_conf_t;
 
-#define LPS22HB_INT_SOURCE     0x25
+#define LPS22HB_INT_SOURCE     0x25U
 typedef struct {
   uint8_t ph               : 1;
   uint8_t pl               : 1;
@@ -220,14 +233,14 @@ typedef struct {
   uint8_t boot_status      : 1;
 } lps22hb_int_source_t;
 
-#define LPS22HB_FIFO_STATUS    0x26
+#define LPS22HB_FIFO_STATUS    0x26U
 typedef struct {
   uint8_t fss              : 6;
   uint8_t ovr              : 1;
   uint8_t fth_fifo         : 1;
 } lps22hb_fifo_status_t;
 
-#define LPS22HB_STATUS         0x27
+#define LPS22HB_STATUS         0x27U
 typedef struct {
   uint8_t p_da             : 1;
   uint8_t t_da             : 1;
@@ -237,12 +250,25 @@ typedef struct {
   uint8_t not_used_01      : 2;
 } lps22hb_status_t;
 
-#define LPS22HB_PRESS_OUT_XL   0x28
-#define LPS22HB_PRESS_OUT_L    0x29
-#define LPS22HB_PRESS_OUT_H    0x2A
-#define LPS22HB_TEMP_OUT_L     0x2B
-#define LPS22HB_TEMP_OUT_H     0x2C
-#define LPS22HB_LPFP_RES       0x33
+#define LPS22HB_PRESS_OUT_XL   0x28U
+#define LPS22HB_PRESS_OUT_L    0x29U
+#define LPS22HB_PRESS_OUT_H    0x2AU
+#define LPS22HB_TEMP_OUT_L     0x2BU
+#define LPS22HB_TEMP_OUT_H     0x2CU
+#define LPS22HB_LPFP_RES       0x33U
+
+/**
+  * @defgroup LPS22HB_Register_Union
+  * @brief    This union group all the registers that has a bit-field
+  *           description.
+  *           This union is useful but not need by the driver.
+  *
+  *           REMOVING this union you are compliant with:
+  *           MISRA-C 2012 [Rule 19.2] -> " Union are not allowed "
+  *
+  * @{
+  *
+  */
 
 typedef union{
   lps22hb_interrupt_cfg_t      interrupt_cfg;
@@ -258,10 +284,18 @@ typedef union{
   uint8_t                      byte;
 } lps22hb_reg_t;
 
+/**
+  * @}
+  *
+  */
+
 int32_t lps22hb_read_reg(lps22hb_ctx_t *ctx, uint8_t reg, uint8_t* data,
                          uint16_t len);
 int32_t lps22hb_write_reg(lps22hb_ctx_t *ctx, uint8_t reg, uint8_t* data,
                           uint16_t len);
+
+extern float_t lps22hb_from_lsb_to_hpa(int16_t lsb);
+extern float_t lps22hb_from_lsb_to_degc(int16_t lsb);
 
 int32_t lps22hb_autozero_rst_set(lps22hb_ctx_t *ctx, uint8_t val);
 int32_t lps22hb_autozero_rst_get(lps22hb_ctx_t *ctx, uint8_t *val);
@@ -283,8 +317,10 @@ typedef enum {
   LPS22HB_LPF_ODR_DIV_9  = 2,
   LPS22HB_LPF_ODR_DIV_20 = 3,
 } lps22hb_lpfp_t;
-int32_t lps22hb_low_pass_filter_mode_set(lps22hb_ctx_t *ctx, lps22hb_lpfp_t val);
-int32_t lps22hb_low_pass_filter_mode_get(lps22hb_ctx_t *ctx, lps22hb_lpfp_t *val);
+int32_t lps22hb_low_pass_filter_mode_set(lps22hb_ctx_t *ctx,
+                                         lps22hb_lpfp_t val);
+int32_t lps22hb_low_pass_filter_mode_get(lps22hb_ctx_t *ctx,
+                                         lps22hb_lpfp_t *val);
 
 typedef enum {
   LPS22HB_POWER_DOWN  = 0,
@@ -345,15 +381,19 @@ typedef enum {
   LPS22HB_NEGATIVE     = 2,
   LPS22HB_BOTH         = 3,
 } lps22hb_pe_t;
-int32_t lps22hb_sign_of_int_threshold_set(lps22hb_ctx_t *ctx, lps22hb_pe_t val);
-int32_t lps22hb_sign_of_int_threshold_get(lps22hb_ctx_t *ctx, lps22hb_pe_t *val);
+int32_t lps22hb_sign_of_int_threshold_set(lps22hb_ctx_t *ctx,
+                                          lps22hb_pe_t val);
+int32_t lps22hb_sign_of_int_threshold_get(lps22hb_ctx_t *ctx,
+                                          lps22hb_pe_t *val);
 
 typedef enum {
   LPS22HB_INT_PULSED  = 0,
   LPS22HB_INT_LATCHED = 1,
 } lps22hb_lir_t;
-int32_t lps22hb_int_notification_mode_set(lps22hb_ctx_t *ctx, lps22hb_lir_t val);
-int32_t lps22hb_int_notification_mode_get(lps22hb_ctx_t *ctx, lps22hb_lir_t *val);
+int32_t lps22hb_int_notification_mode_set(lps22hb_ctx_t *ctx,
+                                          lps22hb_lir_t val);
+int32_t lps22hb_int_notification_mode_get(lps22hb_ctx_t *ctx,
+                                          lps22hb_lir_t *val);
 
 int32_t lps22hb_int_generation_set(lps22hb_ctx_t *ctx, uint8_t val);
 int32_t lps22hb_int_generation_get(lps22hb_ctx_t *ctx, uint8_t *val);
@@ -449,13 +489,14 @@ int32_t lps22hb_auto_add_inc_set(lps22hb_ctx_t *ctx, uint8_t val);
 int32_t lps22hb_auto_add_inc_get(lps22hb_ctx_t *ctx, uint8_t *val);
 
 /**
-  * @}
+  *@}
+  *
   */
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /*__LPS22HB_DRIVER__H */
+#endif /* LPS22HB_REGS_H */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
