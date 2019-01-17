e6e42bceeb2b976df8d35f170d2513673e041f01
albezanc <alberto.zancanato@st.com>
2019-01-17 11:32:56 +0100

    lps33hw: example updates - update        read_data_simple.c

--- lps33hw_STdC/example/read_data_simple.c
+++ lps33hw_STdC/example/read_data_simple.c
@@ -1,38 +1,39 @@
 /*
  ******************************************************************************
  * @file    read_data_simple.c
- * @author  MEMS Software Solution Team
- * @date    10-November-2017
+ * @author  Sensors Software Solution Team
  * @brief   This file show the simplest way to get data from sensor.
  *
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; COPYRIGHT(c) 2017 STMicroelectronics</center></h2>
+ * <h2><center>&copy; COPYRIGHT(c) 2019 STMicroelectronics</center></h2>
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *   1. Redistributions of source code must retain the above copyright notice,
  *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright notice,
- *      this list of conditions and the following disclaimer in the documentation
- *      and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its contributors
- *      may be used to endorse or promote products derived from this software
- *      without specific prior written permission.
+ *   2. Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *   3. Neither the name of STMicroelectronics nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  *
- */
+*/
 
 /* Includes ------------------------------------------------------------------*/
 #include "..\..\stdc\lps33hw_STdC\driver\lps33hw_reg.h"
@@ -212,7 +213,7 @@ void example_main(void)
     {
       memset(data_raw_pressure.u8bit, 0x00, sizeof(int32_t));
       lps33hw_pressure_raw_get(&dev_ctx, data_raw_pressure.u8bit);
-      pressure_hPa = LPS33HW_FROM_LSB_TO_hPa( data_raw_pressure.i32bit);
+      pressure_hPa = lps33hw_from_lsb_to_hpa( data_raw_pressure.i32bit);
       
       sprintf((char*)tx_buffer, "pressure [hPa]:%6.2f\r\n", pressure_hPa);
       tx_com( tx_buffer, strlen( (char const*)tx_buffer ) );
@@ -222,7 +223,7 @@ void example_main(void)
     {
       memset(data_raw_temperature.u8bit, 0x00, sizeof(int16_t));
       lps33hw_temperature_raw_get(&dev_ctx, data_raw_temperature.u8bit);
-      temperature_degC = LPS33HW_FROM_LSB_TO_degC( data_raw_temperature.i16bit );
+      temperature_degC = lps33hw_from_lsb_to_degc( data_raw_temperature.i16bit );
       
       sprintf((char*)tx_buffer, "temperature [degC]:%6.2f\r\n", temperature_degC );
       tx_com( tx_buffer, strlen( (char const*)tx_buffer ) );
