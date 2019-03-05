6c9a8f913888da90daba9f6ef1deed47d4a80e1d
albezanc <alberto.zancanato@st.com>
2019-03-05 16:02:04 +0100

    lps22hh: example updates - updated   read_data_simple.c

--- lps22hh_STdC/example/read_data_simple.c
+++ lps22hh_STdC/example/read_data_simple.c
@@ -38,8 +38,8 @@
 #include "lps22hh_reg.h"
 #include <string.h>
 
-#define MKI109V2
-//#define NUCLEO_STM32F411RE
+//#define MKI109V2
+#define NUCLEO_STM32F411RE
 
 #ifdef MKI109V2
 #include "stm32f1xx_hal.h"
@@ -212,7 +212,7 @@ void example_main(void)
     {
       memset(data_raw_pressure.u8bit, 0x00, sizeof(int32_t));
       lps22hh_pressure_raw_get(&dev_ctx, data_raw_pressure.u8bit);
-      pressure_hPa = LPS22HH_FROM_LSB_TO_hPa( data_raw_pressure.i32bit);
+      pressure_hPa = lps22hh_from_lsb_to_hpa( data_raw_pressure.i32bit);
       
       sprintf((char*)tx_buffer, "pressure [hPa]:%6.2f\r\n", pressure_hPa);
       tx_com( tx_buffer, strlen( (char const*)tx_buffer ) );
@@ -222,7 +222,7 @@ void example_main(void)
     {
       memset(data_raw_temperature.u8bit, 0x00, sizeof(int16_t));
       lps22hh_temperature_raw_get(&dev_ctx, data_raw_temperature.u8bit);
-      temperature_degC = LPS22HH_FROM_LSB_TO_degC( data_raw_temperature.i16bit );
+      temperature_degC = lps22hh_from_lsb_to_celsius( data_raw_temperature.i16bit );
       
       sprintf((char*)tx_buffer, "temperature [degC]:%6.2f\r\n", temperature_degC );
       tx_com( tx_buffer, strlen( (char const*)tx_buffer ) );
