8514261ac2d36062b7972eb6cbda0410d7378a24
albezanc <alberto.zancanato@st.com>
2019-09-18 13:56:34 +0200

    lis2hh12: updated examples
     - read_data_simple.c

--- lis2hh12_STdC/example/read_data_simple.c
+++ lis2hh12_STdC/example/read_data_simple.c
@@ -7,31 +7,15 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; COPYRIGHT(c) 2018 STMicroelectronics</center></h2>
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its
- *      contributors may be used to endorse or promote products derived from
- *      this software without specific prior written permission.
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
+ ******************************************************************************
  */
 
 /*
@@ -81,6 +65,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include <string.h>
+#include <stdio.h>
 #include "stm32f4xx_hal.h"
 #include "lis2hh12_reg.h"
 #include "gpio.h"
@@ -91,6 +76,11 @@
 #elif defined(NUCLEO_F411RE_X_NUCLEO_IKS01A2)
 #include "usart.h"
 #endif
+
+typedef union{
+  int16_t i16bit[3];
+  uint8_t u8bit[6];
+} axis3bit16_t;
 
 /* Private macro -------------------------------------------------------------*/
 
@@ -120,7 +110,7 @@ static void platform_init(void);
 void example_main_lis2hh12(void)
 {
   /*  Initialize mems driver interface */
-  lis2hh12_ctx_t dev_ctx;
+  stmdev_ctx_t dev_ctx;
   dev_ctx.write_reg = platform_write;
   dev_ctx.read_reg = platform_read;
   dev_ctx.handle = &SENSOR_BUS;
@@ -145,15 +135,15 @@ void example_main_lis2hh12(void)
   /* Enable Block Data Update */
   lis2hh12_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
 
-  /* Set full scale */  
+  /* Set full scale */ 
   lis2hh12_xl_full_scale_set(&dev_ctx, LIS2HH12_8g);
 
   /* Configure filtering chain */
-  /* Accelerometer data output- filter path / bandwidth */  
+  /* Accelerometer data output- filter path / bandwidth */ 
   lis2hh12_xl_filter_aalias_bandwidth_set(&dev_ctx, LIS2HH12_AUTO);
   lis2hh12_xl_filter_out_path_set(&dev_ctx, LIS2HH12_FILT_LP);
   lis2hh12_xl_filter_low_bandwidth_set(&dev_ctx, LIS2HH12_LP_ODR_DIV_400);
-  /* Accelerometer interrrupt - filter path / bandwidth */ 
+  /* Accelerometer interrrupt - filter path / bandwidth */
   lis2hh12_xl_filter_int_path_set(&dev_ctx, LIS2HH12_HP_DISABLE);
 
   /* Set Output Data Rate */
@@ -174,11 +164,11 @@ void example_main_lis2hh12(void)
       acceleration_mg[0] = lis2hh12_from_fs8g_to_mg(data_raw_acceleration.i16bit[0]);
       acceleration_mg[1] = lis2hh12_from_fs8g_to_mg(data_raw_acceleration.i16bit[1]);
       acceleration_mg[2] = lis2hh12_from_fs8g_to_mg(data_raw_acceleration.i16bit[2]);
-      
+     
       sprintf((char*)tx_buffer, "Acceleration [mg]:%4.2f\t%4.2f\t%4.2f\r\n",
               acceleration_mg[0], acceleration_mg[1], acceleration_mg[2]);
       tx_com(tx_buffer, strlen((char const*)tx_buffer));
-    } 
+    }
   }
 }
 
