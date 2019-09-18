9dbb8733046be320894e6668855edcff3e241bd0
albezanc <alberto.zancanato@st.com>
2019-09-18 10:39:05 +0200

    ais2dw12: updated examples
     - activity.c
     - free_fall.c
     - orientation_6d.c
     - read_data_simple.c
     - self_test.c
     - wake_up.c

--- ais2dw12_STdC/example/read_data_simple.c
+++ ais2dw12_STdC/example/read_data_simple.c
@@ -7,31 +7,15 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; COPYRIGHT(c) 2019 STMicroelectronics</center></h2>
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its
- *      contributors may be used to endorse or promote products derived from
- *      this software without specific prior written permission.
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
+ ******************************************************************************
  */
 
 /*
@@ -81,6 +65,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include <string.h>
+#include <stdio.h>
 #include "stm32f4xx_hal.h"
 #include "ais2dw12_reg.h"
 #include "gpio.h"
@@ -91,6 +76,11 @@
 #elif defined(NUCLEO_F411RE_X_NUCLEO_IKS01A2)
 #include "usart.h"
 #endif
+
+typedef union{
+  int16_t i16bit[3];
+  uint8_t u8bit[6];
+} axis3bit16_t;
 
 /* Private macro -------------------------------------------------------------*/
 
@@ -122,7 +112,7 @@ void example_main_ais2dw12(void)
   /*
    *  Initialize mems driver interface
    */
-  ais2dw12_ctx_t dev_ctx;
+  stmdev_ctx_t dev_ctx;
   dev_ctx.write_reg = platform_write;
   dev_ctx.read_reg = platform_read;
   dev_ctx.handle = &hi2c1;
@@ -157,20 +147,20 @@ void example_main_ais2dw12(void)
 
   /*
    * Set full scale
-   */  
+   */ 
   ais2dw12_full_scale_set(&dev_ctx, AIS2DW12_2g);
 
   /*
    * Configure filtering chain
    *
    * Accelerometer - filter path / bandwidth
-   */  
+   */ 
   ais2dw12_filter_path_set(&dev_ctx, AIS2DW12_LPF_ON_OUT);
   ais2dw12_filter_bandwidth_set(&dev_ctx, AIS2DW12_ODR_DIV_4);
 
   /*
    * Configure power mode
-   */    
+   */   
   ais2dw12_power_mode_set(&dev_ctx, AIS2DW12_PWR_MD_12bit);
 
   /*
@@ -199,11 +189,11 @@ void example_main_ais2dw12(void)
       acceleration_mg[0] = ais2dw12_from_fs2_12bit_to_mg(data_raw_acceleration.i16bit[0]);
       acceleration_mg[1] = ais2dw12_from_fs2_12bit_to_mg(data_raw_acceleration.i16bit[1]);
       acceleration_mg[2] = ais2dw12_from_fs2_12bit_to_mg(data_raw_acceleration.i16bit[2]);
-      
+     
       sprintf((char*)tx_buffer, "Acceleration [mg]:%4.2f\t%4.2f\t%4.2f\r\n",
               acceleration_mg[0], acceleration_mg[1], acceleration_mg[2]);
       tx_com(tx_buffer, strlen((char const*)tx_buffer));
-    } 
+    }
   }
 }
 
--- ais2dw12_STdC/example/self_test.c
+++ ais2dw12_STdC/example/self_test.c
@@ -7,31 +7,15 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; COPYRIGHT(c) 2019 STMicroelectronics</center></h2>
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its
- *      contributors may be used to endorse or promote products derived from
- *      this software without specific prior written permission.
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
+ ******************************************************************************
  */
 
 /*
@@ -81,6 +65,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include <string.h>
+#include <stdio.h>
 #include "stm32f4xx_hal.h"
 #include "ais2dw12_reg.h"
 #include "gpio.h"
@@ -91,6 +76,11 @@
 #elif defined(NUCLEO_F411RE_X_NUCLEO_IKS01A2)
 #include "usart.h"
 #endif
+
+typedef union{
+  int16_t i16bit[3];
+  uint8_t u8bit[6];
+} axis3bit16_t;
 
 /* Private macro -------------------------------------------------------------*/
 /* Self-test recommended samples */
@@ -128,7 +118,7 @@ static inline float ABSF(float _x)
   return (_x < 0.0f) ? -(_x) : _x;
 }
 
-static int flush_samples(ais2dw12_ctx_t *dev_ctx)
+static int flush_samples(stmdev_ctx_t *dev_ctx)
 {
   ais2dw12_reg_t reg;
   axis3bit16_t dummy;
@@ -147,7 +137,7 @@ static int flush_samples(ais2dw12_ctx_t *dev_ctx)
   return samples;
 }
 
-static void test_self_test_ais2dw12(ais2dw12_ctx_t *dev_ctx)
+static void test_self_test_ais2dw12(stmdev_ctx_t *dev_ctx)
 {
   ais2dw12_reg_t reg;
   float media[3] = { 0.0f, 0.0f, 0.0f };
@@ -276,7 +266,7 @@ void example_main_selt_test_ais2dw12(void)
   /*
    * Initialize mems driver interface
   */
-  ais2dw12_ctx_t dev_ctx;
+  stmdev_ctx_t dev_ctx;
 
   dev_ctx.write_reg = platform_write;
   dev_ctx.read_reg = platform_read;
