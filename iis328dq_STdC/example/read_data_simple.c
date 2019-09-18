0f44c8bfa3b5a1c4b2a4f74a76bce87369dcd4f4
albezanc <alberto.zancanato@st.com>
2019-09-18 13:46:57 +0200

    iis328dq: updated examples
     - read_data_simple.c

--- iis328dq_STdC/example/read_data_simple.c
+++ iis328dq_STdC/example/read_data_simple.c
@@ -8,38 +8,24 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; COPYRIGHT(c) 2017 STMicroelectronics</center></h2>
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright notice,
- *      this list of conditions and the following disclaimer in the documentation
- *      and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its contributors
- *      may be used to endorse or promote products derived from this software
- *      without specific prior written permission.
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ ******************************************************************************
  */
 
 /* Includes ------------------------------------------------------------------*/
-#include "..\..\stdc\iis328dq_STdC\driver\iis328dq_reg.h"
+#include "iis328dq_reg.h"
 #include <string.h>
+#include <stdio.h>
 
-#define MKI109V2
-//#define NUCLEO_STM32F411RE
+//#define MKI109V2
+#define NUCLEO_STM32F411RE
 
 #ifdef MKI109V2
 #include "stm32f1xx_hal.h"
@@ -54,6 +40,11 @@
 #include "usart.h"
 #include "gpio.h"
 #endif
+
+typedef union{
+  int16_t i16bit[3];
+  uint8_t u8bit[6];
+} axis3bit16_t;
 
 /* Private macro -------------------------------------------------------------*/
 #ifdef MKI109V2
@@ -98,15 +89,15 @@ static int32_t platform_write(void *handle, uint8_t Reg, uint8_t *Bufp,
   if (handle == &hi2c1)
   {
     /* enable auto incremented in multiple read/write commands */
-    Reg |= 0x80; 
+    Reg |= 0x80;
     HAL_I2C_Mem_Write(handle, IIS328DQ_I2C_ADD_H, Reg,
                       I2C_MEMADD_SIZE_8BIT, Bufp, len, 1000);
   }
-#ifdef MKI109V2  
+#ifdef MKI109V2 
   else if (handle == &hspi2)
   {
     /* enable auto incremented in multiple read/write commands */
-    Reg |= 0x40;    
+    Reg |= 0x40;   
     HAL_GPIO_WritePin(CS_SPI2_GPIO_Port, CS_SPI2_Pin, GPIO_PIN_RESET);
     HAL_SPI_Transmit(handle, &Reg, 1, 1000);
     HAL_SPI_Transmit(handle, Bufp, len, 1000);
@@ -115,7 +106,7 @@ static int32_t platform_write(void *handle, uint8_t Reg, uint8_t *Bufp,
   else if (handle == &hspi1)
   {
     /* enable auto incremented in multiple read/write commands */
-    Reg |= 0x40;     
+    Reg |= 0x40;    
     HAL_GPIO_WritePin(CS_SPI1_GPIO_Port, CS_SPI1_Pin, GPIO_PIN_RESET);
     HAL_SPI_Transmit(handle, &Reg, 1, 1000);
     HAL_SPI_Transmit(handle, Bufp, len, 1000);
@@ -135,7 +126,7 @@ static int32_t platform_read(void *handle, uint8_t Reg, uint8_t *Bufp,
     HAL_I2C_Mem_Read(handle, IIS328DQ_I2C_ADD_H, Reg,
                      I2C_MEMADD_SIZE_8BIT, Bufp, len, 1000);
   }
-#ifdef MKI109V2   
+#ifdef MKI109V2  
   else if (handle == &hspi2)
   {
     /* enable auto incremented in multiple read/write commands */
@@ -148,13 +139,13 @@ static int32_t platform_read(void *handle, uint8_t Reg, uint8_t *Bufp,
   else
   {
     /* enable auto incremented in multiple read/write commands */
-    Reg |= 0xC0;    
+    Reg |= 0xC0;   
     HAL_GPIO_WritePin(CS_RF_GPIO_Port, CS_RF_Pin, GPIO_PIN_RESET);
     HAL_SPI_Transmit(handle, &Reg, 1, 1000);
     HAL_SPI_Receive(handle, Bufp, len, 1000);
     HAL_GPIO_WritePin(CS_RF_GPIO_Port, CS_RF_Pin, GPIO_PIN_SET);
   }
-#endif  
+#endif 
   return 0;
 }
 
@@ -163,10 +154,10 @@ static int32_t platform_read(void *handle, uint8_t Reg, uint8_t *Bufp,
  */
 void tx_com( uint8_t *tx_buffer, uint16_t len )
 {
-  #ifdef NUCLEO_STM32F411RE  
+  #ifdef NUCLEO_STM32F411RE 
   HAL_UART_Transmit( &huart2, tx_buffer, len, 1000 );
   #endif
-  #ifdef MKI109V2  
+  #ifdef MKI109V2 
   CDC_Transmit_FS( tx_buffer, len );
   #endif
 }
@@ -177,10 +168,15 @@ void example_main(void)
   /*
    *  Initialize mems driver interface
    */
-  iis328dq_ctx_t dev_ctx;
+  stmdev_ctx_t dev_ctx;
   dev_ctx.write_reg = platform_write;
   dev_ctx.read_reg = platform_read;
-  dev_ctx.handle = &hspi2;  
+  #ifdef NUCLEO_STM32F411RE
+  dev_ctx.handle = &hi2c1; 
+  #endif
+  #ifdef MKI109V2 
+  dev_ctx.handle = &hspi2;
+  #endif
   /*
    *  Check device ID
    */
@@ -194,11 +190,11 @@ void example_main(void)
   iis328dq_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
   /*
    * Set full scale
-   */  
+   */ 
   iis328dq_full_scale_set(&dev_ctx, IIS328DQ_2g);
   /*
    * Configure filtering chain
-   */  
+   */ 
   /* Accelerometer - High Pass / Slope path */
   iis328dq_hp_path_set(&dev_ctx, IIS328DQ_HP_DISABLE);
   //iis328dq_hp_path_set(&dev_ctx, IIS328DQ_HP_ON_OUT);
@@ -226,10 +222,10 @@ void example_main(void)
       acceleration_mg[0] = IIS328DQ_FROM_FS_2g_TO_mg( data_raw_acceleration.i16bit[0]);
       acceleration_mg[1] = IIS328DQ_FROM_FS_2g_TO_mg( data_raw_acceleration.i16bit[1]);
       acceleration_mg[2] = IIS328DQ_FROM_FS_2g_TO_mg( data_raw_acceleration.i16bit[2]);
-      
+     
       sprintf((char*)tx_buffer, "Acceleration [mg]:%4.2f\t%4.2f\t%4.2f\r\n",
               acceleration_mg[0], acceleration_mg[1], acceleration_mg[2]);
       tx_com( tx_buffer, strlen( (char const*)tx_buffer ) );
-    } 
+    }
   }
 }
