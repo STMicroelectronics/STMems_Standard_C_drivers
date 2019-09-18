e21414f997a8d57aedc3fd1d3a549a12ad117939
albezanc <alberto.zancanato@st.com>
2019-09-18 10:46:07 +0200

    ais3624dq: updated examples
     - read_data_simple.c

--- ais3624dq_STdC/example/read_data_simple.c
+++ ais3624dq_STdC/example/read_data_simple.c
@@ -7,31 +7,15 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; COPYRIGHT(c) 2018 STMicroelectronics</center></h2>
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its
- *      contributors may be used to endorse or promote products derived from
- *      this software without specific prior written permission.
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
+ ******************************************************************************
  */
 
 /*
@@ -81,6 +65,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include <string.h>
+#include <stdio.h>
 #include <stm32f4xx_hal.h>
 #include <ais3624dq_reg.h>
 #include <gpio.h>
@@ -91,6 +76,11 @@
 #elif defined(NUCLEO_F411RE_X_NUCLEO_IKS01A2)
 #include <usart.h>
 #endif
+
+typedef union{
+  int16_t i16bit[3];
+  uint8_t u8bit[6];
+} axis3bit16_t;
 
 /* Private macro -------------------------------------------------------------*/
 
@@ -119,7 +109,7 @@ static void platform_init(void);
 /* Main Example --------------------------------------------------------------*/
 void example_main_ais3624dq(void)
 {
-  ais3624dq_ctx_t dev_ctx;
+  stmdev_ctx_t dev_ctx;
 
   /* Uncomment to use interrupts on drdy */
   //a3g4250d_int2_route_t int2_reg;
@@ -137,21 +127,21 @@ void example_main_ais3624dq(void)
   ais3624dq_device_id_get(&dev_ctx, &whoamI);
   if ( whoamI != AIS3624DQ_ID )
     while(1); /*manage here device not found */
-  
+ 
   /* Enable Block Data Update */
   ais3624dq_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
-  
-  /* Set full scale */  
+ 
+  /* Set full scale */ 
   ais3624dq_full_scale_set(&dev_ctx, AIS3624DQ_6g);
-  
-  /* Configure filtering chain */  
+ 
+  /* Configure filtering chain */ 
   ais3624dq_hp_path_set(&dev_ctx, AIS3624DQ_HP_DISABLE);
   //ais3624dq_hp_path_set(&dev_ctx, AIS3624DQ_HP_ON_OUT);
   //ais3624dq_hp_reset_get(&dev_ctx);
 
   /* Set Output Data Rate */
   ais3624dq_data_rate_set(&dev_ctx, AIS3624DQ_ODR_5Hz);
-  
+ 
   /* Read samples in polling mode (no int) */
   while(1)
   {
@@ -166,11 +156,11 @@ void example_main_ais3624dq(void)
       acceleration_mg[0] = AIS3624DQ_FROM_FS_6g_TO_mg( data_raw_acceleration.i16bit[0]);
       acceleration_mg[1] = AIS3624DQ_FROM_FS_6g_TO_mg( data_raw_acceleration.i16bit[1]);
       acceleration_mg[2] = AIS3624DQ_FROM_FS_6g_TO_mg( data_raw_acceleration.i16bit[2]);
-      
+     
       sprintf((char*)tx_buffer, "Acceleration [mg]:%4.2f\t%4.2f\t%4.2f\r\n",
               acceleration_mg[0], acceleration_mg[1], acceleration_mg[2]);
       tx_com( tx_buffer, strlen( (char const*)tx_buffer ) );
-    } 
+    }
   }
 }
 
