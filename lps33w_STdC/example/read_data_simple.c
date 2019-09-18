c659a364a959f4b96105daafba21410ca6bb3770
albezanc <alberto.zancanato@st.com>
2019-09-18 13:26:30 +0200

    lps33w: updated examples
     - multi_read_fifo.c
     - read_data_simple.c
     - read_one_shot.c

--- lps33w_STdC/example/read_data_simple.c
+++ lps33w_STdC/example/read_data_simple.c
@@ -7,31 +7,15 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; COPYRIGHT(c) 2018 STMicroelectronics</center></h2>
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its
- *      contributors may be used to endorse or promote products derived from
- *      this software without specific prior written permission.
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
+ ******************************************************************************
  */
 
 /*
@@ -63,8 +47,8 @@
  * If a different hardware is used please comment all
  * following target board and redefine yours.
  */
-#define STEVAL_MKI109V3
-//#define NUCLEO_F411RE_X_NUCLEO_IKS01A2
+//#define STEVAL_MKI109V3
+#define NUCLEO_F411RE_X_NUCLEO_IKS01A2
 
 #if defined(STEVAL_MKI109V3)
 /* MKI109V3: Define communication interface */
@@ -81,6 +65,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include <string.h>
+#include <stdio.h>
 #include "stm32f4xx_hal.h"
 #include <lps33w_reg.h>
 #include "gpio.h"
@@ -92,6 +77,16 @@
 #include "usart.h"
 #endif
 
+typedef union{
+  int16_t i16bit;
+  uint8_t u8bit[2];
+} axis1bit16_t;
+
+typedef union{
+  int32_t i32bit;
+  uint8_t u8bit[4];
+} axis1bit32_t;
+
 /* Private macro -------------------------------------------------------------*/
 
 /* Private variables ---------------------------------------------------------*/
@@ -101,7 +96,7 @@ static float pressure_hPa;
 static float temperature_degC;
 static uint8_t whoamI, rst;
 static uint8_t tx_buffer[1000];
-static lps33w_ctx_t dev_ctx;
+static stmdev_ctx_t dev_ctx;
 
 /* Extern variables ----------------------------------------------------------*/
 
@@ -141,7 +136,7 @@ void example_main_lps33w(void)
   do {
     lps33w_reset_get(&dev_ctx, &rst);
   } while (rst);
-  
+ 
   /* Enable Block Data Update */
   //lps33w_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
 
@@ -153,24 +148,24 @@ void example_main_lps33w(void)
 
   /* Set Output Data Rate */
   lps33w_data_rate_set(&dev_ctx, LPS33W_ODR_10_Hz);
-  
+ 
   /* Read samples in polling mode (no int) */
   while(1)
   {
     uint8_t reg;
-    
+   
     /* Read output only if new value is available */
     lps33w_press_data_ready_get(&dev_ctx, &reg);
     if (reg)
     {
       memset(data_raw_pressure.u8bit, 0x00, sizeof(int32_t));
       lps33w_pressure_raw_get(&dev_ctx, data_raw_pressure.u8bit);
-      
+     
       pressure_hPa = lps33w_from_lsb_to_hpa(data_raw_pressure.i32bit);
       sprintf((char*)tx_buffer, "pressure [hPa]:%6.2f\r\n", pressure_hPa);
 
       tx_com(tx_buffer, strlen((char const*)tx_buffer));
- 
+
       memset(data_raw_temperature.u8bit, 0x00, sizeof(int16_t));
       lps33w_temperature_raw_get(&dev_ctx, data_raw_temperature.u8bit);
 
@@ -179,7 +174,7 @@ void example_main_lps33w(void)
 
       tx_com(tx_buffer, strlen((char const*)tx_buffer));
     }
-    
+   
   }
 }
 
