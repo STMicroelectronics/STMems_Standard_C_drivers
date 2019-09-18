4b84fcc7c48ce0a470450b36c788a63dea9fdc06
albezanc <alberto.zancanato@st.com>
2019-09-18 14:07:31 +0200

    lps33hw: updated examples
     - read_data_simple.c

--- lps33hw_STdC/example/read_data_simple.c
+++ lps33hw_STdC/example/read_data_simple.c
@@ -103,7 +103,7 @@ static int32_t platform_write(void *handle, uint8_t Reg, uint8_t *Bufp,
     HAL_I2C_Mem_Write(handle, LPS33HW_I2C_ADD_H, Reg,
                       I2C_MEMADD_SIZE_8BIT, Bufp, len, 1000);
   }
-#ifdef MKI109V2  
+#ifdef MKI109V2 
   else if (handle == &hspi2)
   {
     HAL_GPIO_WritePin(CS_SPI2_GPIO_Port, CS_SPI2_Pin, GPIO_PIN_RESET);
@@ -130,7 +130,7 @@ static int32_t platform_read(void *handle, uint8_t Reg, uint8_t *Bufp,
       HAL_I2C_Mem_Read(handle, LPS33HW_I2C_ADD_H, Reg,
                        I2C_MEMADD_SIZE_8BIT, Bufp, len, 1000);
   }
-#ifdef MKI109V2   
+#ifdef MKI109V2  
   else if (handle == &hspi2)
   {
     Reg |= 0x80;
@@ -147,7 +147,7 @@ static int32_t platform_read(void *handle, uint8_t Reg, uint8_t *Bufp,
     HAL_SPI_Receive(handle, Bufp, len, 1000);
     HAL_GPIO_WritePin(CS_RF_GPIO_Port, CS_RF_Pin, GPIO_PIN_SET);
   }
-#endif  
+#endif 
   return 0;
 }
 
@@ -156,10 +156,10 @@ static int32_t platform_read(void *handle, uint8_t Reg, uint8_t *Bufp,
  */
 void tx_com( uint8_t *tx_buffer, uint16_t len )
 {
-  #ifdef NUCLEO_STM32F411RE  
+  #ifdef NUCLEO_STM32F411RE 
   HAL_UART_Transmit( &huart2, tx_buffer, len, 1000 );
   #endif
-  #ifdef MKI109V2  
+  #ifdef MKI109V2 
   CDC_Transmit_FS( tx_buffer, len );
   #endif
 }
@@ -174,7 +174,7 @@ void example_main(void)
   lps33hw_ctx_t dev_ctx;
   dev_ctx.write_reg = platform_write;
   dev_ctx.read_reg = platform_read;
-  dev_ctx.handle = &hi2c1;  
+  dev_ctx.handle = &hi2c1; 
   /*
    *  Check device ID
    */
@@ -197,7 +197,7 @@ void example_main(void)
    * Set Output Data Rate
    */
   lps33hw_data_rate_set(&dev_ctx, LPS33HW_ODR_10_Hz);
-  
+ 
   /*
    * Read samples in polling mode (no int)
    */
@@ -214,7 +214,7 @@ void example_main(void)
       memset(data_raw_pressure.u8bit, 0x00, sizeof(int32_t));
       lps33hw_pressure_raw_get(&dev_ctx, data_raw_pressure.u8bit);
       pressure_hPa = lps33hw_from_lsb_to_hpa( data_raw_pressure.i32bit);
-      
+     
       sprintf((char*)tx_buffer, "pressure [hPa]:%6.2f\r\n", pressure_hPa);
       tx_com( tx_buffer, strlen( (char const*)tx_buffer ) );
     }
@@ -224,7 +224,7 @@ void example_main(void)
       memset(data_raw_temperature.u8bit, 0x00, sizeof(int16_t));
       lps33hw_temperature_raw_get(&dev_ctx, data_raw_temperature.u8bit);
       temperature_degC = lps33hw_from_lsb_to_degc( data_raw_temperature.i16bit );
-      
+     
       sprintf((char*)tx_buffer, "temperature [degC]:%6.2f\r\n", temperature_degC );
       tx_com( tx_buffer, strlen( (char const*)tx_buffer ) );
     }
