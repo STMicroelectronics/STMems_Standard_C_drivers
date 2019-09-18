9d002706c68165b915c9a0536747a00960550d75
albezanc <alberto.zancanato@st.com>
2019-09-18 10:41:24 +0200

    a3g4250d: updated examples
     - fifo_stream.c
     - read_data_simple.c
     - wake_up.c

--- a3g4250d_STdC/example/read_data_simple.c
+++ a3g4250d_STdC/example/read_data_simple.c
@@ -7,31 +7,15 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; COPYRIGHT(c) 2018 STMicroelectronics</center></h2>
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *   1. Redistributions of source code must retain the above copyright notice,
- *      this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in the
- *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of STMicroelectronics nor the names of its
- *      contributors may be used to endorse or promote products derived from
- *      this software without specific prior written permission.
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
+ ******************************************************************************
  */
 
 /*
@@ -81,6 +65,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include <string.h>
+#include <stdio.h>
 #include "stm32f4xx_hal.h"
 #include "a3g4250d_reg.h"
 #include "gpio.h"
@@ -91,6 +76,11 @@
 #elif defined(NUCLEO_F411RE_X_NUCLEO_IKS01A2)
 #include "usart.h"
 #endif
+
+typedef union{
+  int16_t i16bit[3];
+  uint8_t u8bit[6];
+} axis3bit16_t;
 
 /* Private macro -------------------------------------------------------------*/
 
@@ -119,7 +109,7 @@ static void platform_init(void);
 /* Main Example --------------------------------------------------------------*/
 void example_main_a3g4250d(void)
 {
-  a3g4250d_ctx_t dev_ctx;
+  stmdev_ctx_t dev_ctx;
 
   /*
    * Uncomment to use interrupts on drdy
@@ -149,7 +139,7 @@ void example_main_a3g4250d(void)
    * Configure filtering chain
    *
    * Gyroscope - High Pass
-   */  
+   */ 
   a3g4250d_filter_path_set(&dev_ctx, A3G4250D_LPF1_HP_ON_OUT);
   a3g4250d_hp_bandwidth_set(&dev_ctx, A3G4250D_HP_LEVEL_3);
 
@@ -185,11 +175,11 @@ void example_main_a3g4250d(void)
       angular_rate_mdps[0] = a3g4250d_from_fs245dps_to_mdps(data_raw_angular_rate.i16bit[0]);
       angular_rate_mdps[1] = a3g4250d_from_fs245dps_to_mdps(data_raw_angular_rate.i16bit[1]);
       angular_rate_mdps[2] = a3g4250d_from_fs245dps_to_mdps(data_raw_angular_rate.i16bit[2]);
-      
+     
       sprintf((char*)tx_buffer, "Angular Rate [mdps]:%4.2f\t%4.2f\t%4.2f\r\n",
               angular_rate_mdps[0], angular_rate_mdps[1], angular_rate_mdps[2]);
       tx_com(tx_buffer, strlen((char const*)tx_buffer));
-    } 
+    }
   }
 }
 
