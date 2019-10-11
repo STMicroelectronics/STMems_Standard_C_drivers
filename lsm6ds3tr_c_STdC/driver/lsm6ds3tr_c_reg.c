6dd5372d24471455a8aac6ad512b5c80e1d5e127
albezanc <alberto.zancanato@st.com>
2019-10-11 11:37:59 +0200

    lsm6ds3tr_c: added an empty line to the end of the files.
    Some compilers issues some WARNINGS if the last file
    line is not empty.

--- lsm6ds3tr_c_STdC/driver/lsm6ds3tr_c_reg.c
+++ lsm6ds3tr_c_STdC/driver/lsm6ds3tr_c_reg.c
@@ -6792,4 +6792,14 @@ int32_t lsm6ds3tr_c_sh_slave_3_dec_get(stmdev_ctx_t *ctx,
   return ret;
 }
 
+/**
+  * @}
+  *
+  */
+  
+/**
+  * @}
+  *
+  */
+
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
