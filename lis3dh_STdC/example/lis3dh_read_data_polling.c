fde7680bce978fac22755deee396444a104af564
albezanc <alberto.zancanato@st.com>
2020-03-12 15:45:32 +0100

    lis3dh: updated examples
    - deleted    read_data_simple.c
    - added      _read_data_polling.c
    - added      _self_test.c

--- lis3dh_STdC/example/lis3dh_read_data_polling.c
+++ lis3dh_STdC/example/lis3dh_read_data_polling.c
@@ -1,13 +1,14 @@
 /*
  ******************************************************************************
- * @file    read_data_simple.c
+ * @file    read_data_polling.c
  * @author  Sensors Software Solution Team
- * @brief   This file show the simplest way to get data from sensor.
+ * @brief   This file shows how to extract data from the sensor in 
+ *          polling mode.
  *
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
  * All rights reserved.</center></h2>
  *
  * This software component is licensed by ST under BSD 3-Clause license,
@@ -22,8 +23,8 @@
  * This example was developed using the following STMicroelectronics
  * evaluation boards:
  *
- * - STEVAL_MKI109V3
- * - NUCLEO_F411RE + X_NUCLEO_IKS01A2
+ * - STEVAL_MKI109V3 + STEVAL-MKI105V1
+ * - NUCLEO_F411RE + X_NUCLEO_IKS01A3 + STEVAL-MKI105V1
  *
  * and STM32CubeMX tool with STM32CubeF4 MCU Package
  *
@@ -32,8 +33,8 @@
  * STEVAL_MKI109V3    - Host side:   USB (Virtual COM)
  *                    - Sensor side: SPI(Default) / I2C(supported)
  *
- * NUCLEO_STM32F411RE + X_NUCLEO_IKS01A2 - Host side: UART(COM) to USB bridge
- *                                       - I2C(Default) / SPI(N/A)
+ * NUCLEO_STM32F411RE - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I2C(Default) / SPI(supported)
  *
  * If you need to run this example on a different hardware platform a
  * modification of the functions: `platform_write`, `platform_read`,
@@ -48,7 +49,7 @@
  * following target board and redefine yours.
  */
 //#define STEVAL_MKI109V3
-#define NUCLEO_F411RE_X_NUCLEO_IKS01A2
+#define NUCLEO_F411RE
 
 #if defined(STEVAL_MKI109V3)
 /* MKI109V3: Define communication interface */
@@ -57,8 +58,8 @@
 /* MKI109V3: Vdd and Vddio power supply values */
 #define PWM_3V3 915
 
-#elif defined(NUCLEO_F411RE_X_NUCLEO_IKS01A2)
-/* NUCLEO_F411RE_X_NUCLEO_IKS01A2: Define communication interface */
+#elif defined(NUCLEO_F411RE)
+/* NUCLEO_F411RE: Define communication interface */
 #define SENSOR_BUS hi2c1
 
 #endif
@@ -73,25 +74,20 @@
 #if defined(STEVAL_MKI109V3)
 #include "usbd_cdc_if.h"
 #include "spi.h"
-#elif defined(NUCLEO_F411RE_X_NUCLEO_IKS01A2)
+#elif defined(NUCLEO_F411RE)
 #include "usart.h"
 #endif
-  
+
 typedef union{
   int16_t i16bit[3];
   uint8_t u8bit[6];
 } axis3bit16_t;
 
-typedef union{
-  uint16_t u16bit;
-  uint8_t  u8bit[2];
-} axis1bit16_t;
-
 /* Private macro -------------------------------------------------------------*/
 
 /* Private variables ---------------------------------------------------------*/
 static axis3bit16_t data_raw_acceleration;
-static axis1bit16_t data_raw_temperature;
+static uint16_t data_raw_temperature;
 static float acceleration_mg[3];
 static float temperature_degC;
 static uint8_t whoamI;
@@ -111,75 +107,55 @@ static int32_t platform_write(void *handle, uint8_t reg, uint8_t *bufp,
 static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
                              uint16_t len);
 static void tx_com(uint8_t *tx_buffer, uint16_t len);
+static void platform_delay(uint32_t ms);
 static void platform_init(void);
 
 /* Main Example --------------------------------------------------------------*/
-void example_main_lis3dh(void)
+void lis3dh_read_data_polling(void)
 {
-  /*
-   *  Initialize mems driver interface
-   */
+  /* Initialize mems driver interface */
   stmdev_ctx_t dev_ctx;
 
   dev_ctx.write_reg = platform_write;
   dev_ctx.read_reg = platform_read;
-  dev_ctx.handle = &hi2c1; 
+  dev_ctx.handle = &SENSOR_BUS; 
 
-  /*
-   * Initialize platform specific hardware
-   */
+  /* Wait boot time and initialize platform specific hardware */
   platform_init();
 
-  /*
-   *  Check device ID
-   */
+  /* Wait sensor boot time */
+  platform_delay(5);
+
+  /* Check device ID */
   lis3dh_device_id_get(&dev_ctx, &whoamI);
-  if (whoamI != LIS3DH_ID)
-  {
-    while(1)
-    {
+  if (whoamI != LIS3DH_ID){
+    while(1) {
       /* manage here device not found */
     }
   }
 
-  /*
-   *  Enable Block Data Update
-   */
+  /* Enable Block Data Update. */
   lis3dh_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
 
-  /*
-   * Set Output Data Rate to 1Hz
-   */
+  /* Set Output Data Rate to 1Hz. */
   lis3dh_data_rate_set(&dev_ctx, LIS3DH_ODR_1Hz);
 
-  /*
-   * Set full scale to 2g
-   */ 
+  /* Set full scale to 2g. */
   lis3dh_full_scale_set(&dev_ctx, LIS3DH_2g);
 
-  /*
-   * Enable temperature sensor
-   */  
+  /* Enable temperature sensor. */
   lis3dh_aux_adc_set(&dev_ctx, LIS3DH_AUX_ON_TEMPERATURE);
 
-  /*
-   * Set device in continuous mode with 12 bit resol.
-   */  
+  /* Set device in continuous mode with 12 bit resol. */
   lis3dh_operating_mode_set(&dev_ctx, LIS3DH_HR_12bit);
  
-  /*
-   * Read samples in polling mode (no int)
-   */
-  while(1)
-  {
+  /* Read samples in polling mode (no int) */
+  while(1) {
     lis3dh_reg_t reg;
 
-    /*
-     * Read output only if new value available
-     */
+    /* Read output only if new value available */
     lis3dh_xl_data_ready_get(&dev_ctx, &reg.byte);
-    if (reg.byte)
-    {
+    if (reg.byte) {
       /* Read accelerometer data */
       memset(data_raw_acceleration.u8bit, 0x00, 3*sizeof(int16_t));
       lis3dh_acceleration_raw_get(&dev_ctx, data_raw_acceleration.u8bit);
@@ -195,14 +171,13 @@ void example_main_lis3dh(void)
       tx_com(tx_buffer, strlen((char const*)tx_buffer));
     }
    
-    lis3dh_temp_data_ready_get(&dev_ctx, &reg.byte);     
-    if (reg.byte)     
-    {
+    lis3dh_temp_data_ready_get(&dev_ctx, &reg.byte);
+    if (reg.byte) {
       /* Read temperature data */
-      memset(data_raw_temperature.u8bit, 0x00, sizeof(int16_t));
-      lis3dh_temperature_raw_get(&dev_ctx, &data_raw_temperature.u16bit);
+      memset(&data_raw_temperature, 0x00, sizeof(int16_t));
+      lis3dh_temperature_raw_get(&dev_ctx, &data_raw_temperature);
       temperature_degC =
-        lis3dh_from_lsb_hr_to_celsius(data_raw_temperature.u16bit);
+        lis3dh_from_lsb_hr_to_celsius(data_raw_temperature);
       
       sprintf((char*)tx_buffer,
               "Temperature [degC]:%6.2f\r\n",
@@ -225,16 +200,14 @@ void example_main_lis3dh(void)
 static int32_t platform_write(void *handle, uint8_t reg, uint8_t *bufp,
                               uint16_t len)
 {
-  if (handle == &hi2c1)
-  {
+  if (handle == &hi2c1) {
     /* Write multiple command */
     reg |= 0x80;
     HAL_I2C_Mem_Write(handle, LIS3DH_I2C_ADD_L, reg,
                       I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
   }
-#ifdef STEVAL_MKI109V3
-  else if (handle == &hspi2)
-  {
+#if defined(STEVAL_MKI109V3)
+  else if (handle == &hspi2) {
     /* Write multiple command */
     reg |= 0x40;
     HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
@@ -259,16 +232,14 @@ static int32_t platform_write(void *handle, uint8_t reg, uint8_t *bufp,
 static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
                              uint16_t len)
 {
-  if (handle == &hi2c1)
-  {
+  if (handle == &hi2c1) {
     /* Read multiple command */
     reg |= 0x80;
     HAL_I2C_Mem_Read(handle, LIS3DH_I2C_ADD_L, reg,
                      I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
   }
-#ifdef STEVAL_MKI109V3
-  else if (handle == &hspi2)
-  {
+#if defined(STEVAL_MKI109V3)
+  else if (handle == &hspi2) {
     /* Read multiple command */
     reg |= 0xC0;
     HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
@@ -289,12 +260,19 @@ static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
  */
 static void tx_com(uint8_t *tx_buffer, uint16_t len)
 {
-  #ifdef NUCLEO_F411RE_X_NUCLEO_IKS01A2
-  HAL_UART_Transmit(&huart2, tx_buffer, len, 1000);
-  #endif
-  #ifdef STEVAL_MKI109V3
+#if defined(STEVAL_MKI109V3)
   CDC_Transmit_FS(tx_buffer, len);
-  #endif
+#elif defined(NUCLEO_F411RE)
+  HAL_UART_Transmit(&huart2, tx_buffer, len, 1000);
+#endif
+}
+
+/*
+ * @brief  platform specific delay (platform dependent)
+ */
+static void platform_delay(uint32_t ms)
+{
+  HAL_Delay(ms);
 }
 
 /*
@@ -302,9 +280,12 @@ static void tx_com(uint8_t *tx_buffer, uint16_t len)
  */
 static void platform_init(void)
 {
-#ifdef STEVAL_MKI109V3
+#if defined(STEVAL_MKI109V3)
   TIM3->CCR1 = PWM_3V3;
   TIM3->CCR2 = PWM_3V3;
   HAL_Delay(1000);
 #endif
+  /* Sensor boot time: 5ms */
+  HAL_Delay(5);
 }
+
