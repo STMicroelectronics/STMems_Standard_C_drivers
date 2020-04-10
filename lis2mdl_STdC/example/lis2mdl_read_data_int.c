3f582ad94f0c9d7ebe6f4bd903164337ffa0fec8
albezanc <alberto.zancanato@st.com>
2020-04-10 13:02:33 +0200

    lis2mdl: examples updated
     - modified     _hard_iron.c
     - modified     _int_conf.c
     - modified     _read_data_int.c
     - modified     _read_data_polling.c
     - modified     _read_data_single.c
     - modified     _self_test.c

--- lis2mdl_STdC/example/lis2mdl_read_data_int.c
+++ lis2mdl_STdC/example/lis2mdl_read_data_int.c
@@ -303,6 +303,8 @@ static void platform_init(void)
 #ifdef STEVAL_MKI109V3
   TIM3->CCR1 = PWM_3V3;
   TIM3->CCR2 = PWM_3V3;
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
   HAL_Delay(1000);
 #endif
 }
