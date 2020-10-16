8b0a80e46df4a68102b6e0a4967e053e59e7019f
albezanc <alberto.zancanato@st.com>
2020-10-16 11:43:18 +0200

    lis2dw12: fixed indentation

--- lis2dw12_STdC/driver/lis2dw12_reg.h
+++ lis2dw12_STdC/driver/lis2dw12_reg.h
@@ -131,9 +131,9 @@ typedef struct {
 /** @defgroup    Generic address-data structure definition
   * @brief       This structure is useful to load a predefined configuration
   *              of a sensor.
-	*              You can create a sensor configuration by your own or using 
-	*              Unico / Unicleo tools available on STMicroelectronics
-	*              web site.
+  *              You can create a sensor configuration by your own or using 
+  *              Unico / Unicleo tools available on STMicroelectronics
+  *              web site.
   *
   * @{
   *
