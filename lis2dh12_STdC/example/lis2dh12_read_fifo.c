328a52c81ba2dec3e329e78a9e8119e17ed64c21
albezanc <alberto.zancanato@st.com>
2021-01-28 10:07:27 +0100

    lis2dh12: added example read_fifo

--- lis2dh12_STdC/example/lis2dh12_read_fifo.c
+++ lis2dh12_STdC/example/lis2dh12_read_fifo.c
@@ -1,14 +1,13 @@
 /*
  ******************************************************************************
- * @file    read_data_polling.c
+ * @file    read_fifo.c
  * @author  Sensors Software Solution Team
- * @brief   This file shows how to extract data from the sensor in
- *          polling mode.
+ * @brief   This file show how to get data from sensor FIFO.
  *
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+ * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
  * All rights reserved.</center></h2>
  *
  * This software component is licensed by ST under BSD 3-Clause license,
@@ -99,21 +98,22 @@
 #include "components.h"
 #endif
 
-
 /* Private macro -------------------------------------------------------------*/
-#define    BOOT_TIME            5 //ms
+#define    BOOT_TIME         5 //ms
+
+/* Define FIFO watermark to 10 samples */
+#define FIFO_WATERMARK     10
 
 /* Private variables ---------------------------------------------------------*/
 static int16_t data_raw_acceleration[3];
-static int16_t data_raw_temperature;
 static float acceleration_mg[3];
-static float temperature_degC;
 static uint8_t whoamI;
 static uint8_t tx_buffer[1000];
 
 /* Extern variables ----------------------------------------------------------*/
 
 /* Private functions ---------------------------------------------------------*/
+
 /*
  *   WARNING:
  *   Functions declare in this section are defined at the end of this file
@@ -125,19 +125,20 @@ static int32_t platform_write(void *handle, uint8_t reg,
                               uint16_t len);
 static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
                              uint16_t len);
-static void tx_com(uint8_t *tx_buffer, uint16_t len);
+static void tx_com( uint8_t *tx_buffer, uint16_t len );
 static void platform_delay(uint32_t ms);
 static void platform_init(void);
 
 /* Main Example --------------------------------------------------------------*/
-void lis2dh12_read_data_polling(void)
+void lis2dh12_read_fifo(void)
 {
-  /* Initialize mems driver interface */
   stmdev_ctx_t dev_ctx;
+  uint8_t dummy;
+  /* Initialize mems driver interface */
   dev_ctx.write_reg = platform_write;
   dev_ctx.read_reg = platform_read;
   dev_ctx.handle = &SENSOR_BUS;
-  /* Wait boot time and initialize platform specific hardware */
+  /* Initialize platform specific hardware */
   platform_init();
   /* Wait sensor boot time */
   platform_delay(BOOT_TIME);
@@ -145,56 +146,49 @@ void lis2dh12_read_data_polling(void)
   lis2dh12_device_id_get(&dev_ctx, &whoamI);
 
   if (whoamI != LIS2DH12_ID) {
-    while (1) {
-      /* manage here device not found */
-    }
+    while (1);/* manage here device not found */
   }
 
+  /* Set FIFO watermark to FIFO_WATERMARK */
+  lis2dh12_fifo_watermark_set(&dev_ctx, FIFO_WATERMARK - 1);
+  /* Set FIFO mode to Stream mode (aka Continuous Mode) */
+  lis2dh12_fifo_mode_set(&dev_ctx, LIS2DH12_DYNAMIC_STREAM_MODE);
+  /* Enable FIFO */
+  lis2dh12_fifo_set(&dev_ctx, PROPERTY_ENABLE);
   /* Enable Block Data Update. */
   lis2dh12_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
   /* Set Output Data Rate to 1Hz. */
-  lis2dh12_data_rate_set(&dev_ctx, LIS2DH12_ODR_1Hz);
+  lis2dh12_data_rate_set(&dev_ctx, LIS2DH12_ODR_25Hz);
   /* Set full scale to 2g. */
   lis2dh12_full_scale_set(&dev_ctx, LIS2DH12_2g);
-  /* Enable temperature sensor. */
-  lis2dh12_temperature_meas_set(&dev_ctx, LIS2DH12_TEMP_ENABLE);
   /* Set device in continuous mode with 12 bit resol. */
   lis2dh12_operating_mode_set(&dev_ctx, LIS2DH12_HR_12bit);
 
-  /* Read samples in polling mode (no int) */
+  /* Wait Events */
   while (1) {
-    lis2dh12_reg_t reg;
-    /* Read output only if new value available */
-    lis2dh12_xl_data_ready_get(&dev_ctx, &reg.byte);
+    /* Check if FIFO level over threshold */
+    lis2dh12_fifo_fth_flag_get(&dev_ctx, &dummy);
 
-    if (reg.byte) {
-      /* Read accelerometer data */
-      memset(data_raw_acceleration, 0x00, 3 * sizeof(int16_t));
-      lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
-      acceleration_mg[0] =
-        lis2dh12_from_fs2_hr_to_mg(data_raw_acceleration[0]);
-      acceleration_mg[1] =
-        lis2dh12_from_fs2_hr_to_mg(data_raw_acceleration[1]);
-      acceleration_mg[2] =
-        lis2dh12_from_fs2_hr_to_mg(data_raw_acceleration[2]);
-      sprintf((char *)tx_buffer,
-              "Acceleration [mg]:%4.2f\t%4.2f\t%4.2f\r\n",
-              acceleration_mg[0], acceleration_mg[1], acceleration_mg[2]);
-      tx_com(tx_buffer, strlen((char const *)tx_buffer));
-    }
+    if (dummy) {
+      /* Read number of sample in FIFO */
+      lis2dh12_fifo_data_level_get(&dev_ctx, &dummy);
 
-    lis2dh12_temp_data_ready_get(&dev_ctx, &reg.byte);
-
-    if (reg.byte) {
-      /* Read temperature data */
-      memset(&data_raw_temperature, 0x00, sizeof(int16_t));
-      lis2dh12_temperature_raw_get(&dev_ctx, &data_raw_temperature);
-      temperature_degC =
-        lis2dh12_from_lsb_hr_to_celsius(data_raw_temperature);
-      sprintf((char *)tx_buffer,
-              "Temperature [degC]:%6.2f\r\n",
-              temperature_degC);
-      tx_com(tx_buffer, strlen((char const *)tx_buffer));
+      while ( dummy > 0) {
+        /* Read XL samples */
+        lis2dh12_acceleration_raw_get(&dev_ctx, data_raw_acceleration);
+        acceleration_mg[0] =
+          lis2dh12_from_fs2_hr_to_mg(data_raw_acceleration[0]);
+        acceleration_mg[1] =
+          lis2dh12_from_fs2_hr_to_mg(data_raw_acceleration[1]);
+        acceleration_mg[2] =
+          lis2dh12_from_fs2_hr_to_mg(data_raw_acceleration[2]);
+        sprintf((char *)tx_buffer,
+                "%d - Acceleration [mg]:%4.2f\t%4.2f\t%4.2f\r\n",
+                FIFO_WATERMARK - dummy,
+                acceleration_mg[0], acceleration_mg[1], acceleration_mg[2]);
+        tx_com(tx_buffer, strlen((char const *)tx_buffer));
+        dummy--;
+      }
     }
   }
 }
