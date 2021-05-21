5a4d9daa20eee16b4099581a8bdfc89ba0bf567d
albezanc <alberto.zancanato@st.com>
2021-05-21 13:05:10 +0200

    ism330dhcx: added const to stmdev_write_ptr #121

--- ism330dhcx_STdC/examples/ism330dhcx_sensor_hub_iis2mdc.c
+++ ism330dhcx_STdC/examples/ism330dhcx_sensor_hub_iis2mdc.c
@@ -119,12 +119,10 @@ static stmdev_ctx_t mag_ctx;
 /* Private functions ---------------------------------------------------------*/
 
 static int32_t ism330dhcx_write_iis2mdc_cx(void *ctx, uint8_t reg,
-                                           uint8_t *data,
-                                           uint16_t len);
+                                           const uint8_t *data, uint16_t len);
 
 static int32_t ism330dhcx_read_iis2mdc_cx(void *ctx, uint8_t reg,
-                                          uint8_t *data,
-                                          uint16_t len);
+                                          uint8_t *data, uint16_t len);
 
 /*
  *   WARNING:
@@ -132,8 +130,7 @@ static int32_t ism330dhcx_read_iis2mdc_cx(void *ctx, uint8_t reg,
  *   and are strictly related to the hardware platform used.
  *
  */
-static int32_t platform_write(void *handle, uint8_t reg,
-                              uint8_t *bufp,
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
                               uint16_t len);
 static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
                              uint16_t len);
@@ -278,8 +275,7 @@ void ism330dhcx_mlc_iis2mdc(void)
  *
  */
 static int32_t ism330dhcx_write_iis2mdc_cx(void *ctx, uint8_t reg,
-                                           uint8_t *data,
-                                           uint16_t len)
+                                           const uint8_t *data, uint16_t len)
 {
   int16_t data_raw_acceleration[3];
   int32_t ret;
@@ -379,20 +375,20 @@ static int32_t ism330dhcx_read_iis2mdc_cx(void *ctx, uint8_t reg,
  * @param  len       number of consecutive register to write
  *
  */
-static int32_t platform_write(void *handle, uint8_t reg,
-                              uint8_t *bufp,
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
                               uint16_t len)
 {
 #if defined(NUCLEO_F411RE)
   HAL_I2C_Mem_Write(handle, ISM330DHCX_I2C_ADD_H, reg,
-                    I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+                    I2C_MEMADD_SIZE_8BIT, (uint8_t*) bufp, len, 1000);
 #elif defined(STEVAL_MKI109V3)
   HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
   HAL_SPI_Transmit(handle, &reg, 1, 1000);
-  HAL_SPI_Transmit(handle, bufp, len, 1000);
+  HAL_SPI_Transmit(handle, (uint8_t*) bufp, len, 1000);
   HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
 #elif defined(SPC584B_DIS)
-  i2c_lld_write(handle,  ISM330DHCX_I2C_ADD_H & 0xFE, reg, bufp, len);
+  i2c_lld_write(handle,  ISM330DHCX_I2C_ADD_H & 0xFE,
+               (uint8_t*) reg, bufp, len);
 #endif
   return 0;
 }
