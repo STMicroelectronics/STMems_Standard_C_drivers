2eb738912912647f853874e065bb0fd8136bf1f3
albezanc <alberto.zancanato@st.com>
2021-05-21 13:05:18 +0200

    lsm303ah: added const to stmdev_write_ptr #121

--- lsm303ah_STdC/examples/lsm303ah_read_data_polling.c
+++ lsm303ah_STdC/examples/lsm303ah_read_data_polling.c
@@ -148,8 +148,7 @@ static uint8_t whoamI, rst;
  *   and are strictly related to the hardware platform used.
  *
  */
-static int32_t platform_write(void *handle, uint8_t reg,
-                              uint8_t *bufp,
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
                               uint16_t len);
 static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
                              uint16_t len);
@@ -272,8 +271,7 @@ void lsm303ah_read_data_polling(void)
  * @param  len       number of consecutive register to write
  *
  */
-static int32_t platform_write(void *handle, uint8_t reg,
-                              uint8_t *bufp,
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
                               uint16_t len)
 {
   sensbus_t *sensbus = (sensbus_t *)handle;
@@ -281,18 +279,18 @@ static int32_t platform_write(void *handle, uint8_t reg,
   /* Write multiple command */
   reg |= 0x80;
   HAL_I2C_Mem_Write(sensbus->hbus, sensbus->i2c_address, reg,
-                    I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+                    I2C_MEMADD_SIZE_8BIT, (uint8_t*) bufp, len, 1000);
 #elif defined(STEVAL_MKI109V3)
   /* Write multiple command */
   reg |= 0x40;
   HAL_GPIO_WritePin(sensbus->cs_port, sensbus->cs_pin, GPIO_PIN_RESET);
   HAL_SPI_Transmit(sensbus->hbus, &reg, 1, 1000);
-  HAL_SPI_Transmit(sensbus->hbus, bufp, len, 1000);
+  HAL_SPI_Transmit(sensbus->hbus, (uint8_t*) bufp, len, 1000);
   HAL_GPIO_WritePin(sensbus->cs_port, sensbus->cs_pin, GPIO_PIN_SET);
 #elif defined(SPC584B_DIS)
   reg |= 0x80;
-  i2c_lld_write(sensbus->hbus, sensbus->i2c_address & 0xFE, reg, bufp,
-                len);
+  i2c_lld_write(sensbus->hbus, sensbus->i2c_address & 0xFE, reg,
+               (uint8_t*) bufp, len);
 #endif
   return 0;
 }
