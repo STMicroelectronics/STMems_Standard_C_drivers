cd90018e39b840fdf7449beca6b6b89079714bb8
albezanc <alberto.zancanato@st.com>
2021-05-21 13:05:13 +0200

    lis2dw12: added const to stmdev_write_ptr #121

--- lis2dw12_STdC/examples/lis2dw12_read_data_single.c
+++ lis2dw12_STdC/examples/lis2dw12_read_data_single.c
@@ -119,8 +119,7 @@ static lis2dw12_ctrl4_int1_pad_ctrl_t  ctrl4_int1_pad;
  *   and are strictly related to the hardware platform used.
  *
  */
-static int32_t platform_write(void *handle, uint8_t reg,
-                              uint8_t *bufp,
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
                               uint16_t len);
 static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
                              uint16_t len);
@@ -216,20 +215,19 @@ void lis2dw12_read_data_single(void)
  * @param  len       number of consecutive register to write
  *
  */
-static int32_t platform_write(void *handle, uint8_t reg,
-                              uint8_t *bufp,
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
                               uint16_t len)
 {
 #if defined(NUCLEO_F411RE)
   HAL_I2C_Mem_Write(handle, LIS2DW12_I2C_ADD_H, reg,
-                    I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+                    I2C_MEMADD_SIZE_8BIT, (uint8_t*) bufp, len, 1000);
 #elif defined(STEVAL_MKI109V3)
   HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
   HAL_SPI_Transmit(handle, &reg, 1, 1000);
-  HAL_SPI_Transmit(handle, bufp, len, 1000);
+  HAL_SPI_Transmit(handle, (uint8_t*) bufp, len, 1000);
   HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
 #elif defined(SPC584B_DIS)
-  i2c_lld_write(handle,  LIS2DW12_I2C_ADD_H & 0xFE, reg, bufp, len);
+  i2c_lld_write(handle,  LIS2DW12_I2C_ADD_H & 0xFE, reg, (uint8_t*) bufp, len);
 #endif
   return 0;
 }
