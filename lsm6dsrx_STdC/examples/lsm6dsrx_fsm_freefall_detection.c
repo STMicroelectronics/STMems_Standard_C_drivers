28cef4c00d99b26eba8c62cb1a2c66bcf1d4e754
Armando Visconti <armando.visconti@st.com>
2022-03-09 17:25:40 +0100

    lsm6dsrx examples: add FSM freefall detection
    Add freefall detection example running on Finite State Machine.
    The example is loading following ucf file:
      "_resources/STMems_Finite_State_Machine/application_examples/lsm6dsrx/Freefall detection/lsm6dsrx_freefall_detection.h"
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

---
+++ lsm6dsrx_STdC/examples/lsm6dsrx_fsm_freefall_detection.c
@@ -0,0 +1,296 @@
+/*
+ ******************************************************************************
+ * @file    lsm6dsrx_fsm_freefall_detection.c
+ * @author  Sensors Software Solution Team
+ * @brief   The purpose of this example is to show how use the device
+ *          Finite State Machine (FSM) starting from an ".h" file
+ *          generated through with the tool "Finite State Machine"
+ *          of Unico GUI.
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+
+/*
+ * Some FSM examples are available at:
+ * https://github.com/STMicroelectronics/STMems_Finite_State_Machine
+ * the same repository is linked to this repository in folder "_resources"
+ *
+ * For more information about Finite State Machine tool please refer
+ * to AN5389 "LSM6DSRX: Finite State Machine".
+ *
+ * This example was developed using the following STMicroelectronics
+ * evaluation boards:
+ *
+ * - STEVAL_MKI109V3 + STEVAL-MKI197V1
+ * - NUCLEO_F411RE + STEVAL-MKI197V1
+ * - DISCOVERY_SPC584B + STEVAL-MKI197V1
+ *
+ * Used interfaces:
+ *
+ * STEVAL_MKI109V3    - Host side:   USB (Virtual COM)
+ *                    - Sensor side: SPI(Default) / I2C(supported)
+ *
+ * NUCLEO_STM32F411RE - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I2C(Default) / SPI(supported)
+ *
+ * DISCOVERY_SPC584B  - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I2C(Default) / SPI(supported)
+ *
+ * If you need to run this example on a different hardware platform a
+ * modification of the functions: `platform_write`, `platform_read`,
+ * `tx_com` and 'platform_init' is required.
+ *
+ */
+
+/* STMicroelectronics evaluation boards definition
+ *
+ * Please uncomment ONLY the evaluation boards in use.
+ * If a different hardware is used please comment all
+ * following target board and redefine yours.
+ */
+
+//#define STEVAL_MKI109V3  /* little endian */
+//#define NUCLEO_F411RE    /* little endian */
+//#define SPC584B_DIS      /* big endian */
+
+/* ATTENTION: By default the driver is little endian. If you need switch
+ *            to big endian please see "Endianness definitions" in the
+ *            header file of the driver (_reg.h).
+ */
+
+#if defined(STEVAL_MKI109V3)
+/* MKI109V3: Define communication interface */
+#define SENSOR_BUS hspi2
+/* MKI109V3: Vdd and Vddio power supply values */
+#define PWM_3V3 915
+
+#elif defined(NUCLEO_F411RE)
+/* NUCLEO_F411RE: Define communication interface */
+#define SENSOR_BUS hi2c1
+
+#elif defined(SPC584B_DIS)
+/* DISCOVERY_SPC584B: Define communication interface */
+#define SENSOR_BUS I2CD1
+
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <string.h>
+#include <stdio.h>
+
+#include "lsm6dsrx_freefall_detection.h"
+#include "lsm6dsrx_reg.h"
+
+#if defined(NUCLEO_F411RE)
+#include "stm32f4xx_hal.h"
+#include "usart.h"
+#include "gpio.h"
+#include "i2c.h"
+
+#elif defined(STEVAL_MKI109V3)
+#include "stm32f4xx_hal.h"
+#include "usbd_cdc_if.h"
+#include "gpio.h"
+#include "spi.h"
+#include "tim.h"
+
+#elif defined(SPC584B_DIS)
+#include "components.h"
+#endif
+
+/* Private macro -------------------------------------------------------------*/
+#define    BOOT_TIME            10 //ms
+
+/* Private variables ---------------------------------------------------------*/
+static uint8_t whoamI, rst;
+static uint8_t tx_buffer[1000];
+
+/* Extern variables ----------------------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+
+/*
+ *   WARNING:
+ *   Functions declare in this section are defined at the end of this file
+ *   and are strictly related to the hardware platform used.
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len);
+static void platform_delay(uint32_t ms);
+static void tx_com( uint8_t *tx_buffer, uint16_t len );
+static void platform_init(void);
+
+/* Main Example --------------------------------------------------------------*/
+void lsm6dsrx_fsm_freefall_detection(void)
+{
+  /* Variable declaration */
+  lsm6dsrx_pin_int1_route_t pin_int1_route;
+  lsm6dsrx_all_sources_t status;
+  stmdev_ctx_t dev_ctx;
+  uint32_t i;
+  /* Initialize mems driver interface */
+  dev_ctx.write_reg = platform_write;
+  dev_ctx.read_reg  = platform_read;
+  dev_ctx.handle    = &SENSOR_BUS;
+  /* Init test platform */
+  platform_init();
+  /* Wait sensor boot time */
+  platform_delay(BOOT_TIME);
+  /* Check device ID */
+  lsm6dsrx_device_id_get(&dev_ctx, &whoamI);
+
+  if (whoamI != LSM6DSRX_ID)
+    while (1);
+
+  /* Restore default configuration */
+  lsm6dsrx_reset_set(&dev_ctx, PROPERTY_ENABLE);
+
+  do {
+    lsm6dsrx_reset_get(&dev_ctx, &rst);
+  } while (rst);
+
+  /* Start Machine Learning Core configuration */
+  for ( i = 0; i < (sizeof(lsm6dsrx_freefall_detection) /
+                    sizeof(ucf_line_t) ); i++ ) {
+    lsm6dsrx_write_reg(&dev_ctx, lsm6dsrx_freefall_detection[i].address,
+                       (uint8_t *)&lsm6dsrx_freefall_detection[i].data, 1);
+  }
+
+  /* Route signals on interrupt pin 1 */
+  lsm6dsrx_pin_int1_route_get(&dev_ctx, &pin_int1_route);
+  pin_int1_route.fsm_int1_a.int1_fsm1               = PROPERTY_ENABLE;
+  lsm6dsrx_pin_int1_route_set(&dev_ctx, &pin_int1_route);
+  /* Configure interrupt pin mode notification */
+  lsm6dsrx_int_notification_set(&dev_ctx,
+                                LSM6DSRX_BASE_PULSED_EMB_LATCHED);
+
+  /* Main loop */
+  while (1) {
+    /* Read interrupt source registers in polling mode (no int) */
+    lsm6dsrx_all_sources_get(&dev_ctx, &status);
+
+    if (status.fsm_status_a.is_fsm1) {
+      sprintf((char *)tx_buffer, "freefall detected\r\n");
+      tx_com(tx_buffer, strlen((char const *)tx_buffer));
+    }
+
+    platform_delay(1);
+  }
+}
+
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to write
+ * @param  bufp      pointer to data to write in register reg
+ * @param  len       number of consecutive register to write
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+#if defined(NUCLEO_F411RE)
+  HAL_I2C_Mem_Write(handle, LSM6DSRX_I2C_ADD_H, reg,
+                    I2C_MEMADD_SIZE_8BIT, (uint8_t*) bufp, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Transmit(handle, (uint8_t*) bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+#elif defined(SPC584B_DIS)
+  i2c_lld_write(handle,  LSM6DSRX_I2C_ADD_H & 0xFE, reg,
+               (uint8_t*) bufp, len);
+#endif
+  return 0;
+}
+
+/*
+ * @brief  Read generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to read
+ * @param  bufp      pointer to buffer that store the data read
+ * @param  len       number of consecutive register to read
+ *
+ */
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+#if defined(NUCLEO_F411RE)
+  HAL_I2C_Mem_Read(handle, LSM6DSRX_I2C_ADD_H, reg,
+                   I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  reg |= 0x80;
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Receive(handle, bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+#elif defined(SPC584B_DIS)
+  i2c_lld_read(handle, LSM6DSRX_I2C_ADD_H & 0xFE, reg, bufp, len);
+#endif
+  return 0;
+}
+
+/*
+ * @brief  Send buffer to console (platform dependent)
+ *
+ * @param  tx_buffer     buffer to transmit
+ * @param  len           number of byte to send
+ *
+ */
+static void tx_com(uint8_t *tx_buffer, uint16_t len)
+{
+#if defined(NUCLEO_F411RE)
+  HAL_UART_Transmit(&huart2, tx_buffer, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  CDC_Transmit_FS(tx_buffer, len);
+#elif defined(SPC584B_DIS)
+  sd_lld_write(&SD2, tx_buffer, len);
+#endif
+}
+
+/*
+ * @brief  platform specific delay (platform dependent)
+ *
+ * @param  ms        delay in ms
+ *
+ */
+static void platform_delay(uint32_t ms)
+{
+#if defined(NUCLEO_F411RE) | defined(STEVAL_MKI109V3)
+  HAL_Delay(ms);
+#elif defined(SPC584B_DIS)
+  osalThreadDelayMilliseconds(ms);
+#endif
+}
+
+/*
+ * @brief  platform specific initialization (platform dependent)
+ */
+static void platform_init(void)
+{
+#if defined(STEVAL_MKI109V3)
+  TIM3->CCR1 = PWM_3V3;
+  TIM3->CCR2 = PWM_3V3;
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_Delay(1000);
+#endif
+}
