6c9a50b114133816a20857c23f845dd0fd998359
Armando Visconti <armando.visconti@st.com>
2022-03-31 16:29:10 +0200

    ilps22qs_STdC: Added ilps22qs driver as submodule
    Added ilps22qs driver as submodule
    (https://github.com/STMicroelectronics/ilps22qs)
    
    Add also some examples.
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

---
+++ ilps22qs_STdC/examples/ilps22qs_read_data_polling.c
@@ -0,0 +1,296 @@
+/*
+ ******************************************************************************
+ * @file    read_data_polling.c
+ * @author  Sensors Software Solution Team
+ * @brief   This file show the simplest way to get data from sensor.
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+
+/*
+ * This example was developed using the following STMicroelectronics
+ * evaluation boards:
+ *
+ * - STEVAL_MKI109V3
+ * - NUCLEO_F411RE
+ * - DISCOVERY_SPC584B
+ *
+ * and STM32CubeMX tool with STM32CubeF4 MCU Package
+ *
+ * Used interfaces:
+ *
+ * STEVAL_MKI109V3    - Host side:   USB (Virtual COM)
+ *                    - Sensor side: SPI(Default) / I2C(supported)
+ *
+ * NUCLEO_STM32F411RE - Host side: UART(COM) to USB bridge
+ *                    - I2C(Default) / SPI(N/A)
+ *
+ * DISCOVERY_SPC584B  - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I2C(Default) / SPI(supported)
+ *
+ * If you need to run this example on a different hardware platform a
+ * modification of the functions: `platform_write`, `platform_read`,
+ * `tx_com` and 'platform_init' is required.
+ *
+ */
+
+/* STMicroelectronics evaluation boards definition
+ *
+ * Please uncomment ONLY the evaluation boards in use.
+ * If a different hardware is used please comment all
+ * following target board and redefine yours.
+ */
+
+//#define STEVAL_MKI109V3  /* little endian */
+//#define NUCLEO_F411RE    /* little endian */
+//#define SPC584B_DIS      /* big endian */
+
+/* ATTENTION: By default the driver is little endian. If you need switch
+ *            to big endian please see "Endianness definitions" in the
+ *            header file of the driver (_reg.h).
+ */
+
+
+#if defined(STEVAL_MKI109V3)
+/* MKI109V3: Define communication interface */
+#define SENSOR_BUS hspi2
+/* MKI109V3: Vdd and Vddio power supply values */
+#define PWM_3V3 915
+
+#elif defined(NUCLEO_F411RE)
+/* NUCLEO_F411RE: Define communication interface */
+#define SENSOR_BUS hi2c1
+
+#elif defined(SPC584B_DIS)
+/* DISCOVERY_SPC584B: Define communication interface */
+#define SENSOR_BUS I2CD1
+
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <string.h>
+#include <stdio.h>
+#include "ilps22qs_reg.h"
+
+#if defined(NUCLEO_F411RE)
+#include "stm32f4xx_hal.h"
+#include "usart.h"
+#include "gpio.h"
+#include "i2c.h"
+
+#elif defined(STEVAL_MKI109V3)
+#include "stm32f4xx_hal.h"
+#include "usbd_cdc_if.h"
+#include "gpio.h"
+#include "spi.h"
+#include "tim.h"
+
+#elif defined(SPC584B_DIS)
+#include "components.h"
+#endif
+
+/* Private macro -------------------------------------------------------------*/
+#define    BOOT_TIME         10 //ms
+
+/* Private variables ---------------------------------------------------------*/
+static uint8_t tx_buffer[1000];
+static ilps22qs_data_t data;
+
+/* Extern variables ----------------------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+/*
+ *   WARNING:
+ *   Functions declare in this section are defined at the end of this file
+ *   and are strictly related to the hardware platform used.
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, uint8_t *bufp,
+                              uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len);
+static void tx_com( uint8_t *tx_buffer, uint16_t len );
+static void platform_delay(uint32_t ms);
+static void platform_init(void);
+
+/* Main Example --------------------------------------------------------------*/
+void ilps22qs_read_data_polling(void)
+{
+  ilps22qs_all_sources_t all_sources;
+  ilps22qs_bus_mode_t bus_mode;
+  ilps22qs_stat_t status;
+  stmdev_ctx_t dev_ctx;
+  ilps22qs_id_t id;
+  ilps22qs_md_t md;
+
+  /* Initialize mems driver interface */
+  dev_ctx.write_reg = platform_write;
+  dev_ctx.read_reg = platform_read;
+  dev_ctx.handle = &SENSOR_BUS;
+
+  /* Initialize platform specific hardware */
+  platform_init();
+
+  /* Wait sensor boot time */
+  platform_delay(BOOT_TIME);
+
+  /* Check device ID */
+  ilps22qs_id_get(&dev_ctx, &id);
+  if (id.whoami != ILPS22QS_ID)
+    while(1);
+
+  /* Restore default configuration */
+  ilps22qs_init_set(&dev_ctx, ILPS22QS_RESET);
+  do {
+    ilps22qs_status_get(&dev_ctx, &status);
+  } while (status.sw_reset);
+
+  /* Disable AH/QVAR to save power consumption */
+  ilps22qs_ah_qvar_disable(&dev_ctx);
+
+  /* Set bdu and if_inc recommended for driver usage */
+  ilps22qs_init_set(&dev_ctx, ILPS22QS_DRV_RDY);
+
+  /* Select bus interface */
+  bus_mode.filter = ILPS22QS_AUTO;
+  bus_mode.interface = ILPS22QS_SEL_BY_HW;
+  ilps22qs_bus_mode_set(&dev_ctx, &bus_mode);
+
+  /* Set Output Data Rate */
+  md.odr = ILPS22QS_4Hz;
+  md.avg = ILPS22QS_16_AVG;
+  md.lpf = ILPS22QS_LPF_ODR_DIV_4;
+  md.fs = ILPS22QS_1260hPa;
+  ilps22qs_mode_set(&dev_ctx, &md);
+
+  /* Read samples in polling mode (no int) */
+  while(1)
+  {
+
+    /* Read output only if new values are available */
+    ilps22qs_all_sources_get(&dev_ctx, &all_sources);
+    if ( all_sources.drdy_pres | all_sources.drdy_temp ) {
+      ilps22qs_data_get(&dev_ctx, &md, &data);
+
+      sprintf((char*)tx_buffer,
+              "pressure [hPa]:%6.2f temperature [degC]:%6.2f\r\n",
+              data.pressure.hpa, data.heat.deg_c);
+
+      tx_com(tx_buffer, strlen((char const*)tx_buffer));
+    }
+
+  }
+}
+
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to write
+ * @param  bufp      pointer to data to write in register reg
+ * @param  len       number of consecutive register to write
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, uint8_t *bufp,
+                              uint16_t len)
+{
+#if defined(NUCLEO_F411RE)
+  HAL_I2C_Mem_Write(handle, ILPS22QS_I2C_ADD, reg,
+                    I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Transmit(handle, bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+#elif defined(SPC584B_DIS)
+  i2c_lld_write(handle,  ILPS22QS_I2C_ADD & 0xFE, reg, bufp, len);
+#endif
+  return 0;
+}
+
+/*
+ * @brief  Read generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to read
+ * @param  bufp      pointer to buffer that store the data read
+ * @param  len       number of consecutive register to read
+ *
+ */
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+#if defined(NUCLEO_F411RE)
+  HAL_I2C_Mem_Read(handle, ILPS22QS_I2C_ADD, reg,
+                   I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  reg |= 0x80;
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Receive(handle, bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+#elif defined(SPC584B_DIS)
+  i2c_lld_read(handle, ILPS22QS_I2C_ADD & 0xFE, reg, bufp, len);
+#endif
+  return 0;
+}
+
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  tx_buffer     buffer to trasmit
+ * @param  len           number of byte to send
+ *
+ */
+static void tx_com(uint8_t *tx_buffer, uint16_t len)
+{
+#if defined(NUCLEO_F411RE)
+  HAL_UART_Transmit(&huart2, tx_buffer, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  CDC_Transmit_FS(tx_buffer, len);
+#elif defined(SPC584B_DIS)
+  sd_lld_write(&SD2, tx_buffer, len);
+#endif
+}
+
+/*
+ * @brief  platform specific delay (platform dependent)
+ *
+ * @param  ms        delay in ms
+ *
+ */
+static void platform_delay(uint32_t ms)
+{
+#if defined(NUCLEO_F411RE) | defined(STEVAL_MKI109V3)
+  HAL_Delay(ms);
+#elif defined(SPC584B_DIS)
+  osalThreadDelayMilliseconds(ms);
+#endif
+}
+
+/*
+ * @brief  platform specific initialization (platform dependent)
+ */
+static void platform_init(void)
+{
+#if defined(STEVAL_MKI109V3)
+  TIM3->CCR1 = PWM_3V3;
+  TIM3->CCR2 = PWM_3V3;
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_Delay(1000);
+#endif
+}
