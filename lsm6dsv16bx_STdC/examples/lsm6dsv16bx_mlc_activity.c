6af9370e2fc303f0d4c96210f5bc7e24f097ba7f
Armando Visconti <armando.visconti@st.com>
2022-12-01 11:18:26 +0100

    lsm6dsv16bx_STdC: add examples for FSM and MLC
    Add few examples in lsm6dsv16bx about how to use the
    FSM and MLC APIs.
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

---
+++ lsm6dsv16bx_STdC/examples/lsm6dsv16bx_mlc_activity.c
@@ -0,0 +1,297 @@
+/*
+ ******************************************************************************
+ * @file    mlc_activity.c
+ * @author  Sensors Software Solution Team
+ * @brief   This file shows how to generate and handle a Free Fall event.
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+
+/*
+ * This example was developed using the following STMicroelectronics
+ * evaluation boards:
+ *
+ * - STEVAL_MKI109V3 +
+ * - NUCLEO_F411RE +
+ * - DISCOVERY_SPC584B +
+ *
+ * Used interfaces:
+ *
+ * STEVAL_MKI109V3    - Host side:   USB (Virtual COM)
+ *                    - Sensor side: SPI(Default) / I2C(supported)
+ *
+ * NUCLEO_STM32F411RE - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I2C(Default) / SPI(supported)
+ *
+ * DISCOVERY_SPC584B  - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I2C(Default) / SPI(supported)
+ *
+ * If you need to run this example on a different hardware platform a
+ * modification of the functions: `platform_write`, `platform_read`,
+ * `tx_com` and 'platform_init' is required.
+ *
+ */
+
+/* STMicroelectronics evaluation boards definition
+ *
+ * Please uncomment ONLY the evaluation boards in use.
+ * If a different hardware is used please comment all
+ * following target board and redefine yours.
+ */
+
+//#define STEVAL_MKI109V3  /* little endian */
+//#define NUCLEO_F411RE    /* little endian */
+//#define SPC584B_DIS      /* big endian */
+
+/* ATTENTION: By default the driver is little endian. If you need switch
+ *            to big endian please see "Endianness definitions" in the
+ *            header file of the driver (_reg.h).
+ */
+
+#if defined(STEVAL_MKI109V3)
+/* MKI109V3: Define communication interface */
+#define SENSOR_BUS hspi2
+/* MKI109V3: Vdd and Vddio power supply values */
+#define PWM_3V3 915
+
+#elif defined(NUCLEO_F411RE)
+/* NUCLEO_F411RE: Define communication interface */
+#define SENSOR_BUS hi2c1
+
+#elif defined(SPC584B_DIS)
+/* DISCOVERY_SPC584B: Define communication interface */
+#define SENSOR_BUS I2CD1
+
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <string.h>
+#include <stdio.h>
+#include "lsm6dsv16bx_activity_recognition_for_wrist.h"
+#include "lsm6dsv16bx_reg.h"
+
+#if defined(NUCLEO_F411RE)
+#include "stm32f4xx_hal.h"
+#include "usart.h"
+#include "gpio.h"
+#include "i2c.h"
+
+#elif defined(STEVAL_MKI109V3)
+#include "stm32f4xx_hal.h"
+#include "usbd_cdc_if.h"
+#include "gpio.h"
+#include "spi.h"
+#include "tim.h"
+
+#elif defined(SPC584B_DIS)
+#include "components.h"
+#endif
+
+/* Private macro -------------------------------------------------------------*/
+#define    BOOT_TIME            10 //ms
+
+/* Private variables ---------------------------------------------------------*/
+static uint8_t whoamI;
+static uint8_t tx_buffer[1000];
+
+/* Extern variables ----------------------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+/*
+ *   WARNING:
+ *   Functions declare in this section are defined at the end of this file
+ *   and are strictly related to the hardware platform used.
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len);
+static void tx_com( uint8_t *tx_buffer, uint16_t len );
+static void platform_delay(uint32_t ms);
+static void platform_init(void);
+
+static stmdev_ctx_t dev_ctx;
+static uint8_t activity_event_catched = 0;
+
+void lsm6dsv16bx_mlc_activity_handler(void)
+{
+  lsm6dsv16bx_all_sources_t status;
+  lsm6dsv16bx_mlc_out_t mlc_out;
+
+  /* Read output only if new xl value is available */
+  lsm6dsv16bx_all_sources_get(&dev_ctx, &status);
+
+  if (status.mlc1) {
+    lsm6dsv16bx_mlc_out_get(&dev_ctx, &mlc_out);
+    activity_event_catched = mlc_out.mlc1_src;
+  }
+}
+
+/* Main Example --------------------------------------------------------------*/
+void lsm6dsv16bx_mlc_activity(void)
+{
+  lsm6dsv16bx_reset_t rst;
+  uint32_t i;
+
+  /* Initialize mems driver interface */
+  dev_ctx.write_reg = platform_write;
+  dev_ctx.read_reg = platform_read;
+  dev_ctx.handle = &SENSOR_BUS;
+  /* Init test platform */
+  platform_init();
+  /* Wait sensor boot time */
+  platform_delay(BOOT_TIME);
+  /* Check device ID */
+  lsm6dsv16bx_device_id_get(&dev_ctx, &whoamI);
+
+  if (whoamI != LSM6DSV16BX_ID)
+    while (1);
+
+  /* Restore default configuration */
+  lsm6dsv16bx_reset_set(&dev_ctx, LSM6DSV16BX_RESTORE_CTRL_REGS);
+  do {
+    lsm6dsv16bx_reset_get(&dev_ctx, &rst);
+  } while (rst != LSM6DSV16BX_READY);
+
+  /* Start Machine Learning Core configuration */
+  for ( i = 0; i < (sizeof(lsm6dsv16bx_activity_recognition_for_wrist) /
+                    sizeof(ucf_line_t) ); i++ ) {
+    lsm6dsv16bx_write_reg(&dev_ctx, lsm6dsv16bx_activity_recognition_for_wrist[i].address,
+                       (uint8_t *)&lsm6dsv16bx_activity_recognition_for_wrist[i].data, 1);
+  }
+
+  /* wait forever (FF event handle in irq handler) */
+  while (1) {
+    if (activity_event_catched != 0x0) {
+      switch(activity_event_catched) {
+      case 1:
+        sprintf((char *)tx_buffer, "Stationary\r\n");
+        tx_com(tx_buffer, strlen((char const *)tx_buffer));
+        break;
+      case 4:
+        sprintf((char *)tx_buffer, "Walking\r\n");
+        tx_com(tx_buffer, strlen((char const *)tx_buffer));
+        break;
+      case 8:
+        sprintf((char *)tx_buffer, "Jogging/Running\r\n");
+        tx_com(tx_buffer, strlen((char const *)tx_buffer));
+        break;
+      }
+      activity_event_catched = 0;
+    }
+  }
+}
+
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to write
+ * @param  bufp      pointer to data to write in register reg
+ * @param  len       number of consecutive register to write
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+#if defined(NUCLEO_F411RE)
+  HAL_I2C_Mem_Write(handle, LSM6DSV16BX_I2C_ADD_L, reg,
+                    I2C_MEMADD_SIZE_8BIT, (uint8_t*) bufp, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Transmit(handle, (uint8_t*) bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+#elif defined(SPC584B_DIS)
+  i2c_lld_write(handle,  LSM6DSV16BX_I2C_ADD_L & 0xFE, reg, (uint8_t*) bufp, len);
+#endif
+  return 0;
+}
+
+/*
+ * @brief  Read generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to read
+ * @param  bufp      pointer to buffer that store the data read
+ * @param  len       number of consecutive register to read
+ *
+ */
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+#if defined(NUCLEO_F411RE)
+  HAL_I2C_Mem_Read(handle, LSM6DSV16BX_I2C_ADD_L, reg,
+                   I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  reg |= 0x80;
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Receive(handle, bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+#elif defined(SPC584B_DIS)
+  i2c_lld_read(handle, LSM6DSV16BX_I2C_ADD_L & 0xFE, reg, bufp, len);
+#endif
+  return 0;
+}
+
+/*
+ * @brief  platform specific outputs on terminal (platform dependent)
+ *
+ * @param  tx_buffer     buffer to transmit
+ * @param  len           number of byte to send
+ *
+ */
+static void tx_com(uint8_t *tx_buffer, uint16_t len)
+{
+#if defined(NUCLEO_F411RE)
+  HAL_UART_Transmit(&huart2, tx_buffer, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  CDC_Transmit_FS(tx_buffer, len);
+#elif defined(SPC584B_DIS)
+  sd_lld_write(&SD2, tx_buffer, len);
+#endif
+}
+
+/*
+ * @brief  platform specific delay (platform dependent)
+ *
+ * @param  ms        delay in ms
+ *
+ */
+static void platform_delay(uint32_t ms)
+{
+#if defined(NUCLEO_F411RE) | defined(STEVAL_MKI109V3)
+  HAL_Delay(ms);
+#elif defined(SPC584B_DIS)
+  osalThreadDelayMilliseconds(ms);
+#endif
+}
+
+/*
+ * @brief  platform specific initialization (platform dependent)
+ */
+static void platform_init(void)
+{
+#if defined(STEVAL_MKI109V3)
+  TIM3->CCR1 = PWM_3V3;
+  TIM3->CCR2 = PWM_3V3;
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_Delay(1000);
+#endif
+}
