89429854247134f9344c4fe4e9b827d9f0839929
Armando Visconti <armando.visconti@st.com>
2022-12-09 15:48:54 +0100

    STdC: Add lsm6dsv driver as submodules
    Add lsm6dsv drivers as submodule, together with few examples
    on how to use APIs.
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

---
+++ lsm6dsv_STdC/examples/lsm6dsv_fifo.c
@@ -0,0 +1,338 @@
+/*
+ ******************************************************************************
+ * @file    fifo_irq.c
+ * @author  Sensors Software Solution Team
+ * @brief   This file how to configure compressed FIFO and to retrieve acc
+ *          and gyro data. This sample use a fifo utility library tool
+ *          for FIFO decompression.
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+
+/*
+ * This example was developed using the following STMicroelectronics
+ * evaluation boards:
+ *
+ * - STEVAL_MKI109V3 + STEVAL-MKI196V1
+ * - NUCLEO_F411RE + X_NUCLEO_IKS01A3
+ * - DISCOVERY_SPC584B + STEVAL-MKI196V1
+ *
+ * Used interfaces:
+ *
+ * STEVAL_MKI109V3    - Host side:   USB (Virtual COM)
+ *                    - Sensor side: SPI(Default) / I2C(supported)
+ *
+ * NUCLEO_STM32F411RE - Host side: UART(COM) to USB bridge
+ *                    - I2C(Default) / SPI(supported)
+ *
+ * DISCOVERY_SPC584B  - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I2C(Default) / SPI(supported)
+ *
+ * If you need to run this example on a different hardware platform a
+ * modification of the functions: `platform_write`, `platform_read`,
+ * `tx_com` and 'platform_init' is required.
+ *
+ */
+
+/* STMicroelectronics evaluation boards definition
+ *
+ * Please uncomment ONLY the evaluation boards in use.
+ * If a different hardware is used please comment all
+ * following target board and redefine yours.
+ */
+
+//#define STEVAL_MKI109V3  /* little endian */
+//#define NUCLEO_F411RE    /* little endian */
+//#define SPC584B_DIS      /* big endian */
+
+/* ATTENTION: By default the driver is little endian. If you need switch
+ *            to big endian please see "Endianness definitions" in the
+ *            header file of the driver (_reg.h).
+ */
+
+#if defined(STEVAL_MKI109V3)
+/* MKI109V3: Define communication interface */
+#define SENSOR_BUS hspi2
+/* MKI109V3: Vdd and Vddio power supply values */
+#define PWM_3V3 915
+
+#elif defined(NUCLEO_F411RE)
+/* NUCLEO_F411RE: Define communication interface */
+#define SENSOR_BUS hi2c1
+
+#elif defined(SPC584B_DIS)
+/* DISCOVERY_SPC584B: Define communication interface */
+#define SENSOR_BUS I2CD1
+
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <string.h>
+#include <stdio.h>
+#include "lsm6dsv_reg.h"
+
+#if defined(NUCLEO_F411RE)
+#include "stm32f4xx_hal.h"
+#include "usart.h"
+#include "gpio.h"
+#include "i2c.h"
+
+#elif defined(STEVAL_MKI109V3)
+#include "stm32f4xx_hal.h"
+#include "usbd_cdc_if.h"
+#include "gpio.h"
+#include "spi.h"
+#include "tim.h"
+
+#elif defined(SPC584B_DIS)
+#include "components.h"
+#endif
+
+/* Private macro -------------------------------------------------------------*/
+/*
+ * Select FIFO samples watermark, max value is 512
+ * in FIFO are stored acc, gyro and timestamp samples
+ */
+#define BOOT_TIME         10
+#define FIFO_WATERMARK    64
+
+/* Private variables ---------------------------------------------------------*/
+static uint8_t whoamI;
+static uint8_t tx_buffer[1000];
+
+/* Private variables ---------------------------------------------------------*/
+static int16_t *datax;
+static int16_t *datay;
+static int16_t *dataz;
+static int32_t *ts;
+
+/* Extern variables ----------------------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+
+/*
+ *   WARNING:
+ *   Functions declare in this section are defined at the end of this file
+ *   and are strictly related to the hardware platform used.
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len);
+static void tx_com( uint8_t *tx_buffer, uint16_t len );
+static void platform_delay(uint32_t ms);
+static void platform_init(void);
+
+static stmdev_ctx_t dev_ctx;
+
+void lsm6dsv_fifo_irq_handler(void)
+{
+  uint16_t num = 0;
+  lsm6dsv_fifo_status_t fifo_status;
+
+  /* Read watermark flag */
+  lsm6dsv_fifo_status_get(&dev_ctx, &fifo_status);
+
+  num = fifo_status.fifo_level;
+  sprintf((char *)tx_buffer, "-- FIFO num %d \r\n", num);
+  tx_com(tx_buffer, strlen((char const *)tx_buffer));
+
+  while (num--) {
+    lsm6dsv_fifo_out_raw_t f_data;
+
+    /* Read FIFO sensor value */
+    lsm6dsv_fifo_out_raw_get(&dev_ctx, &f_data);
+    datax = (int16_t *)&f_data.data[0];
+    datay = (int16_t *)&f_data.data[2];
+    dataz = (int16_t *)&f_data.data[4];
+    ts = (int32_t *)&f_data.data[0];
+
+    switch (f_data.tag) {
+    case LSM6DSV_XL_NC_TAG:
+      sprintf((char *)tx_buffer, "ACC [mg]:\t%4.2f\t%4.2f\t%4.2f\r\n",
+              lsm6dsv_from_fs2_to_mg(*datax),
+              lsm6dsv_from_fs2_to_mg(*datay),
+              lsm6dsv_from_fs2_to_mg(*dataz));
+      tx_com(tx_buffer, strlen((char const *)tx_buffer));
+      break;
+    case LSM6DSV_TIMESTAMP_TAG:
+      sprintf((char *)tx_buffer, "TIMESTAMP [ms] %d\r\n", *ts);
+      tx_com(tx_buffer, strlen((char const *)tx_buffer));
+      break;
+    default:
+      break;
+    }
+  }
+
+  sprintf((char *)tx_buffer, "------ \r\n\r\n");
+  tx_com(tx_buffer, strlen((char const *)tx_buffer));
+}
+
+/* Main Example --------------------------------------------------------------*/
+void lsm6dsv_fifo_irq(void)
+{
+  lsm6dsv_pin_int_route_t pin_int;
+  lsm6dsv_reset_t rst;
+
+  /* Initialize mems driver interface */
+  dev_ctx.write_reg = platform_write;
+  dev_ctx.read_reg = platform_read;
+  dev_ctx.handle = &SENSOR_BUS;
+  /* Init test platform */
+  platform_init();
+  /* Wait sensor boot time */
+  platform_delay(BOOT_TIME);
+
+  /* Check device ID */
+  lsm6dsv_device_id_get(&dev_ctx, &whoamI);
+
+  if (whoamI != LSM6DSV_ID)
+    while (1);
+
+  /* Restore default configuration */
+  lsm6dsv_reset_set(&dev_ctx, LSM6DSV_RESTORE_CTRL_REGS);
+  do {
+    lsm6dsv_reset_get(&dev_ctx, &rst);
+  } while (rst != LSM6DSV_READY);
+
+  /* Enable Block Data Update */
+  lsm6dsv_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
+  /* Set full scale */
+  lsm6dsv_xl_full_scale_set(&dev_ctx, LSM6DSV_2g);
+
+  /*
+   * Set FIFO watermark (number of unread sensor data TAG + 6 bytes
+   * stored in FIFO) to FIFO_WATERMARK samples
+   */
+  lsm6dsv_fifo_watermark_set(&dev_ctx, FIFO_WATERMARK);
+  /* Set FIFO batch XL/Gyro ODR to 12.5Hz */
+  lsm6dsv_fifo_xl_batch_set(&dev_ctx, LSM6DSV_XL_BATCHED_AT_60Hz);
+  /* Set FIFO mode to Stream mode (aka Continuous Mode) */
+  lsm6dsv_fifo_mode_set(&dev_ctx, LSM6DSV_STREAM_MODE);
+
+  pin_int.fifo_th = PROPERTY_ENABLE;
+  lsm6dsv_pin_int1_route_set(&dev_ctx, &pin_int);
+  //lsm6dsv_pin_int2_route_set(&dev_ctx, &pin_int);
+
+  /* Set Output Data Rate */
+  lsm6dsv_xl_data_rate_set(&dev_ctx, LSM6DSV_ODR_AT_60Hz);
+  lsm6dsv_fifo_timestamp_batch_set(&dev_ctx, LSM6DSV_TMSTMP_DEC_32);
+  lsm6dsv_timestamp_set(&dev_ctx, PROPERTY_ENABLE);
+
+  /* wait forever (FIFO samples read with irq) */
+  while (1);
+}
+
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to write
+ * @param  bufp      pointer to data to write in register reg
+ * @param  len       number of consecutive register to write
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+#if defined(NUCLEO_F411RE)
+  HAL_I2C_Mem_Write(handle, LSM6DSV_I2C_ADD_L, reg,
+                    I2C_MEMADD_SIZE_8BIT, (uint8_t*) bufp, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Transmit(handle, (uint8_t*) bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+#elif defined(SPC584B_DIS)
+  i2c_lld_write(handle,  LSM6DSV_I2C_ADD_H & 0xFE, reg, (uint8_t*) bufp, len);
+#endif
+  return 0;
+}
+
+/*
+ * @brief  Read generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to read
+ * @param  bufp      pointer to buffer that store the data read
+ * @param  len       number of consecutive register to read
+ *
+ */
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+#if defined(NUCLEO_F411RE)
+  HAL_I2C_Mem_Read(handle, LSM6DSV_I2C_ADD_L, reg,
+                   I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  reg |= 0x80;
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Receive(handle, bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+#elif defined(SPC584B_DIS)
+  i2c_lld_read(handle, LSM6DSV_I2C_ADD_H & 0xFE, reg, bufp, len);
+#endif
+  return 0;
+}
+
+/*
+ * @brief  Send buffer to console (platform dependent)
+ *
+ * @param  tx_buffer     buffer to transmit
+ * @param  len           number of byte to send
+ *
+ */
+static void tx_com(uint8_t *tx_buffer, uint16_t len)
+{
+#if defined(NUCLEO_F411RE)
+  HAL_UART_Transmit(&huart2, tx_buffer, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  CDC_Transmit_FS(tx_buffer, len);
+#elif defined(SPC584B_DIS)
+  sd_lld_write(&SD2, tx_buffer, len);
+#endif
+}
+
+/*
+ * @brief  platform specific delay (platform dependent)
+ *
+ * @param  ms        delay in ms
+ *
+ */
+static void platform_delay(uint32_t ms)
+{
+#if defined(NUCLEO_F411RE) | defined(STEVAL_MKI109V3)
+  HAL_Delay(ms);
+#elif defined(SPC584B_DIS)
+  osalThreadDelayMilliseconds(ms);
+#endif
+}
+
+/*
+ * @brief  platform specific initialization (platform dependent)
+ */
+static void platform_init(void)
+{
+#if defined(STEVAL_MKI109V3)
+  TIM3->CCR1 = PWM_3V3;
+  TIM3->CCR2 = PWM_3V3;
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_Delay(1000);
+#endif
+}
