e99230ad178e9fddad0263bfc807cef226cda7e2
Armando Visconti <armando.visconti@st.com>
2024-03-07 14:46:26 +0100

    project IAR: mk109V3: protect VDD and VDDIO from dropping down
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- _prj_MKI109V3/Src/main.c
+++ _prj_MKI109V3/Src/main.c
@@ -109,7 +109,8 @@ int main(void)
   MX_USART2_UART_Init();
   MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_GPIO_WritePin(Range_100x_A_GPIO_Port, Range_100x_A_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(Range_100x_B_GPIO_Port, Range_100x_B_Pin, GPIO_PIN_SET);
   /* USER CODE END 2 */
 
   /* Infinite loop */
