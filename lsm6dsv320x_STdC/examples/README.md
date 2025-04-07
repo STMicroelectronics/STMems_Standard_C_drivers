c5ac3d13d55467657c7b9698642f611a5a331777
Armando Visconti <armando.visconti@st.com>
2025-04-07 15:58:15 +0200

    Add driver and examples for lsm6dsv320 device
     - Driver v1.0.0
     - Examples:
          - lsm6dsv320x_fsm_fourd.c
          - lsm6dsv320x_fsm_glance.c
          - lsm6dsv320x_hg_wakeup.c
          - lsm6dsv320x_mlc_gym.c
          - lsm6dsv320x_read_data_drdy.c
          - lsm6dsv320x_read_data_polling.c
          - lsm6dsv320x_read_fifo.c
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

---
+++ lsm6dsv320x_STdC/examples/README.md
@@ -0,0 +1,45 @@
+# Examples
+
+This folder contains application examples. The examples refer to following boards but, since the drivers are platform independent, they can be easily ported to any other platform:
+
+- STEVAL-MKI109V3 (https://www.st.com/en/evaluation-tools/steval-mki109v3.html)
+- NUCLEO-F401RE (https://www.st.com/en/microcontrollers-microprocessors/stm32f401re.html)
+- DISCOVERY-SPC584B (https://www.st.com/en/evaluation-tools/spc584b-dis.html)
+- NUCLEO-H503RB (https://www.st.com/en/evaluation-tools/nucleo-h503rb.html)
+
+## Read data
+
+Read accelerometer, gyroscope and temperature sensor data in polling mode:
+
+  - lsm6dsv320x_read_data_polling.c
+
+Read accelerometer sensor data on INT1 data ready:
+
+  - lsm6dsv320x_read_data_drdy.c
+
+Read accelerometer and gyroscope sensor data from FIFO on FIFO threshold event:
+
+  - lsm6dsv320x_fifo.c
+
+## Program and use embedded digital functions
+
+Program LSM6DSV320X to receive High-g wakeup events and data on INT1:
+
+  - lsm6dsv320x_hg_wakeup.c
+
+## Finite State Machine (FSM)
+
+Program LSM6DSV320X FSM to detect *glance* and *de-glance* gestures typically used in smartphone devices (read more [here](https://github.com/STMicroelectronics/STMems_Finite_State_Machine/blob/master/application_examples/lsm6dsv320x/Glance%20detection/README.md)):
+
+  - lsm6dsv320x_fsm_glance.c
+
+Program LSM6DSV320X FSM to detect 4D position recognition typically used in smartphone devices (read more [here](https://github.com/STMicroelectronics/STMems_Finite_State_Machine/blob/master/application_examples/lsm6dsv320x/FourD%20position%20recognition/README.md)):
+
+  - lsm6dsv320x_fsm_fourd.c
+
+## Machine Learning Core (MLC)
+
+Program LSM6DSV320X MLC for gym activity recognition used on wearable device, such a smartwatch or a wristband. (read more [here](https://github.com/STMicroelectronics/STMems_Machine_Learning_Core/blob/master/application_examples/lsm6dsv320x/gym_activity_recognition/README.md)):
+
+  - lsm6dsv320x_mlc_gym.c
+
---
+++ lsm6dsv320x_STdC/examples/lsm6dsv320x_hg_wakeup.c
@@ -0,0 +1,342 @@
+/*
+ ******************************************************************************
+ * @file    hg_wakeup.c
+ * @author  Sensors Software Solution Team
+ * @brief   This file shows how to get data from sensor.
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+
+/*
+ * This example was developed using the following STMicroelectronics
+ * evaluation boards:
+ *
+ * - STEVAL_MKI109V3 +
+ * - NUCLEO_F401RE +
+ * - DISCOVERY_SPC584B +
+ *
+ * Used interfaces:
+ *
+ * STEVAL_MKI109V3    - Host side:   USB (Virtual COM)
+ *                    - Sensor side: SPI(Default) / I2C(supported)
+ *
+ * NUCLEO_STM32F411RE - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I2C(Default) / SPI(supported)
+ *
+ * DISCOVERY_SPC584B  - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I2C(Default) / SPI(supported)
+ *
+ * If you need to run this example on a different hardware platform a
+ * modification of the functions: `platform_write`, `platform_read`,
+ * `tx_com` and 'platform_init' is required.
+ *
+ */
+
+/* STMicroelectronics evaluation boards definition
+ *
+ * Please uncomment ONLY the evaluation boards in use.
+ * If a different hardware is used please comment all
+ * following target board and redefine yours.
+ */
+
+//#define STEVAL_MKI109V3  /* little endian */
+//#define NUCLEO_F401RE    /* little endian */
+//#define SPC584B_DIS      /* big endian */
+
+/* ATTENTION: By default the driver is little endian. If you need switch
+ *            to big endian please see "Endianness definitions" in the
+ *            header file of the driver (_reg.h).
+ */
+
+#if defined(STEVAL_MKI109V3)
+/* MKI109V3: Define communication interface */
+#define SENSOR_BUS hspi2
+/* MKI109V3: Vdd and Vddio power supply values */
+#define PWM_3V3 915
+
+#elif defined(NUCLEO_F401RE)
+/* NUCLEO_F401RE: Define communication interface */
+#define SENSOR_BUS hi2c1
+
+#elif defined(SPC584B_DIS)
+/* DISCOVERY_SPC584B: Define communication interface */
+#define SENSOR_BUS I2CD1
+
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <string.h>
+#include <stdio.h>
+#include "lsm6dsv320x_reg.h"
+
+#if defined(NUCLEO_F401RE)
+#include "stm32f4xx_hal.h"
+#include "usart.h"
+#include "gpio.h"
+#include "i2c.h"
+
+#elif defined(STEVAL_MKI109V3)
+#include "stm32f4xx_hal.h"
+#include "usbd_cdc_if.h"
+#include "gpio.h"
+#include "spi.h"
+#include "tim.h"
+
+#elif defined(SPC584B_DIS)
+#include "components.h"
+#endif
+
+/* Private macro -------------------------------------------------------------*/
+#define    BOOT_TIME            10 //ms
+#define    CNT_FOR_OUTPUT       100
+
+/* Private variables ---------------------------------------------------------*/
+//static int16_t data_raw_motion[3];
+//static int16_t data_raw_temperature;
+//static float_t acceleration_mg[3];
+//static float_t angular_rate_mdps[3];
+//static float_t temperature_degC;
+static uint8_t whoamI;
+static uint8_t tx_buffer[1000];
+
+static lsm6dsv320x_filt_settling_mask_t filt_settling_mask;
+
+/* Extern variables ----------------------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+
+#define AXIS_X  0x1
+#define AXIS_Y  0x2
+#define AXIS_Z  0x4
+
+/*
+ *   WARNING:
+ *   Functions declare in this section are defined at the end of this file
+ *   and are strictly related to the hardware platform used.
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len);
+static void tx_com( uint8_t *tx_buffer, uint16_t len );
+static void platform_delay(uint32_t ms);
+static void platform_init(void);
+
+static   stmdev_ctx_t dev_ctx;
+static   uint8_t thread_wake = 0;
+
+void lsm6dsv320x_hg_wakeup_handler(void)
+{
+  thread_wake = 1;
+}
+
+/* Main Example --------------------------------------------------------------*/
+void lsm6dsv320x_hg_wakeup(void)
+{
+  lsm6dsv320x_reset_t rst;
+  lsm6dsv320x_pin_int_route_t pin_int = { 0 };
+  lsm6dsv320x_hg_wake_up_cfg_t wu_cfg = { 0 };
+  lsm6dsv320x_hg_wu_interrupt_cfg_t int_cfg = { 0 };
+
+  /* Initialize mems driver interface */
+  dev_ctx.write_reg = platform_write;
+  dev_ctx.read_reg = platform_read;
+  dev_ctx.mdelay = platform_delay;
+  dev_ctx.handle = &SENSOR_BUS;
+  /* Init test platform */
+  platform_init();
+  /* Wait sensor boot time */
+  platform_delay(BOOT_TIME);
+  /* Check device ID */
+  lsm6dsv320x_device_id_get(&dev_ctx, &whoamI);
+
+  if (whoamI != LSM6DSV320X_ID)
+    while (1);
+
+  /* Restore default configuration */
+  lsm6dsv320x_reset_set(&dev_ctx, LSM6DSV320X_RESTORE_CTRL_REGS);
+  do {
+    lsm6dsv320x_reset_get(&dev_ctx, &rst);
+  } while (rst != LSM6DSV320X_READY);
+
+  /* Enable Block Data Update */
+  lsm6dsv320x_block_data_update_set(&dev_ctx, PROPERTY_ENABLE);
+
+  /* Set Output Data Rate.
+   * Selected data rate have to be equal or greater with respect
+   * with MLC data rate.
+   */
+  lsm6dsv320x_hg_xl_data_rate_set(&dev_ctx, LSM6DSV320X_HG_XL_ODR_AT_960Hz, 1);
+
+  /* Set full scale */
+  lsm6dsv320x_hg_xl_full_scale_set(&dev_ctx, LSM6DSV320X_256g);
+
+  /* Configure filtering chain */
+  filt_settling_mask.drdy = PROPERTY_ENABLE;
+  filt_settling_mask.irq_xl = PROPERTY_ENABLE;
+  filt_settling_mask.irq_g = PROPERTY_ENABLE;
+  lsm6dsv320x_filt_settling_mask_set(&dev_ctx, filt_settling_mask);
+  lsm6dsv320x_filt_xl_lp2_set(&dev_ctx, PROPERTY_ENABLE);
+  lsm6dsv320x_filt_xl_lp2_bandwidth_set(&dev_ctx, LSM6DSV320X_XL_STRONG);
+
+  wu_cfg.hg_shock_dur = 1;
+  wu_cfg.hg_wakeup_ths = 4;
+  lsm6dsv320x_hg_wake_up_cfg_set(&dev_ctx, wu_cfg);
+
+  /* enable interrupt on HG wakeup */
+  pin_int.hg_wakeup = PROPERTY_ENABLE;
+  lsm6dsv320x_pin_int1_route_hg_set(&dev_ctx, &pin_int);
+  //lsm6dsv320x_pin_int2_route_hg_set(&dev_ctx, &pin_int);
+
+  int_cfg.hg_interrupts_enable = 1;
+  lsm6dsv320x_hg_wu_interrupt_cfg_set(&dev_ctx, int_cfg);
+
+  /* "thread" loop */
+  while (1) {
+    if (thread_wake) {
+      lsm6dsv320x_hg_event_t status;
+
+      thread_wake = 0;
+
+      lsm6dsv320x_hg_event_get(&dev_ctx, &status);
+
+      if (status.hg_event) {
+        if (status.hg_wakeup) {
+          uint8_t axis = 0;
+
+          if (status.hg_wakeup_x) {
+            axis |= AXIS_X;
+          }
+
+          if (status.hg_wakeup_y) {
+            axis |= AXIS_Y;
+          }
+
+          if (status.hg_wakeup_z) {
+            axis |= AXIS_Z;
+          }
+
+          snprintf((char *)tx_buffer, sizeof(tx_buffer),"WAKEUP event on X: %d, Y = %d, Z= %d\r\n",
+                  (axis & AXIS_X) ? 1 : 0,
+                  (axis & AXIS_Y) ? 1 : 0,
+                  (axis & AXIS_Z) ? 1 : 0);
+          tx_com(tx_buffer, strlen((char const *)tx_buffer));
+        }
+      }
+    }
+  }
+}
+
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to write
+ * @param  bufp      pointer to data to write in register reg
+ * @param  len       number of consecutive register to write
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+#if defined(NUCLEO_F401RE)
+  HAL_I2C_Mem_Write(handle, LSM6DSV320X_I2C_ADD_L, reg,
+                    I2C_MEMADD_SIZE_8BIT, (uint8_t*) bufp, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Transmit(handle, (uint8_t*) bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+#elif defined(SPC584B_DIS)
+  i2c_lld_write(handle,  LSM6DSV320X_I2C_ADD_L & 0xFE, reg, (uint8_t*) bufp, len);
+#endif
+  return 0;
+}
+
+/*
+ * @brief  Read generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to read
+ * @param  bufp      pointer to buffer that store the data read
+ * @param  len       number of consecutive register to read
+ *
+ */
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+#if defined(NUCLEO_F401RE)
+  HAL_I2C_Mem_Read(handle, LSM6DSV320X_I2C_ADD_L, reg,
+                   I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  reg |= 0x80;
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Receive(handle, bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+#elif defined(SPC584B_DIS)
+  i2c_lld_read(handle, LSM6DSV320X_I2C_ADD_L & 0xFE, reg, bufp, len);
+#endif
+  return 0;
+}
+
+/*
+ * @brief  platform specific outputs on terminal (platform dependent)
+ *
+ * @param  tx_buffer     buffer to transmit
+ * @param  len           number of byte to send
+ *
+ */
+static void tx_com(uint8_t *tx_buffer, uint16_t len)
+{
+#if defined(NUCLEO_F401RE)
+  HAL_UART_Transmit(&huart2, tx_buffer, len, 1000);
+#elif defined(STEVAL_MKI109V3)
+  CDC_Transmit_FS(tx_buffer, len);
+#elif defined(SPC584B_DIS)
+  sd_lld_write(&SD2, tx_buffer, len);
+#endif
+}
+
+/*
+ * @brief  platform specific delay (platform dependent)
+ *
+ * @param  ms        delay in ms
+ *
+ */
+static void platform_delay(uint32_t ms)
+{
+#if defined(NUCLEO_F401RE) | defined(STEVAL_MKI109V3)
+  HAL_Delay(ms);
+#elif defined(SPC584B_DIS)
+  osalThreadDelayMilliseconds(ms);
+#endif
+}
+
+/*
+ * @brief  platform specific initialization (platform dependent)
+ */
+static void platform_init(void)
+{
+#if defined(STEVAL_MKI109V3)
+  TIM3->CCR1 = PWM_3V3;
+  TIM3->CCR2 = PWM_3V3;
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_Delay(1000);
+#endif
+}
