28327e124bb038d0abbf0d1bafe36a42310222de
Armando Visconti <armando.visconti@st.com>
2025-12-05 12:07:47 +0100

    asm6g300b: add support for combo4 6-axis device

---
+++ asm6g300b_STdC/driver/asm6g300b_reg.c
@@ -0,0 +1,590 @@
+/*
+ ******************************************************************************
+ * @file    asm6g300b_reg.c
+ * @author  Sensors Software Solution Team
+ * @brief   ASM6G300B driver file
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#include <string.h>
+#include "asm6g300b_reg.h"
+
+/**
+  * @defgroup    ASM6G300B
+  * @brief       This file provides a set of functions needed to drive the
+  *              asm6g300b sensor.
+  * @{
+  *
+  */
+
+/**
+  * @defgroup    ASM6G300B_Interfaces_Functions
+  * @brief       This section provide a set of functions used to read and
+  *              write a generic register of the device and prepare SafeSPI
+  *              MISO and MOSI frames.
+  *              MANDATORY: return 0 -> no Error.
+  * @{
+  *
+  */
+
+/*
+ * Calculate the CRC3 of a frame.
+ */
+static uint8_t asm6g300b_calc_crc3(uint32_t data, uint8_t init, uint8_t poly)
+{
+  uint8_t crc = init;
+
+  for (int i = 31; i >= 0; --i)
+  {
+    // Bring in the next data bit
+    uint8_t bit = (data >> i) & 1;
+
+    // Shift in the data bit
+    crc = ((crc << 1) | bit);
+
+    // If the new MSB (bit 3) is set, apply the polynomial
+    if (crc & 0x8)
+    {
+      crc ^= poly;
+    }
+
+    // Keep CRC to 3 bits (mask off the 4th bit)
+    crc &= 0x7;
+  }
+
+  return crc;
+}
+
+/*
+ * Generate SafeSPI MOSI frame
+ */
+static uint32_t asm6g300b_gen_mosi_frame(uint8_t ta, uint8_t rw, uint16_t data)
+{
+  uint32_t frame, crc3;
+
+  frame = (ta << 22) | (rw << 21) | (data << 3);
+
+  /* poly is (x^3 + x^1 + x^0) */
+  crc3 = asm6g300b_calc_crc3(frame & ~0x7, 0x05, 0xB);
+
+  return (frame | crc3);
+}
+
+/*
+ * Decode SafeSPI MISO frame
+ */
+static int32_t asm6g300b_dec_miso_frame(uint32_t frame, uint16_t *data, uint8_t *state)
+{
+  uint32_t crc3;
+  uint8_t s1, s0;
+
+  /* poly is (x^3 + x^1 + x^0) */
+  crc3 = asm6g300b_calc_crc3(frame & ~0x7, 0x05, 0xB);
+  if (crc3 != (frame & 0x7))
+  {
+    return -1;
+  }
+
+  s1 = (frame >> 20) & 0x1;
+  s0 = (frame >> 3) & 0x1;
+  *state = (s1 << 1) | s0;
+  *data = (frame >> 4) & 0xffff;
+
+  return 0;
+}
+
+/*
+ * Read frame from SafeSPI bus
+ */
+static int32_t asm6g300b_recv_rsp_frame(const stmdev_ctx_t *ctx, uint8_t *reg, uint8_t *data)
+{
+  uint32_t frame;
+  uint16_t d;
+  uint8_t s;
+  int ret;
+
+  if (ctx == NULL)
+  {
+    return -1;
+  }
+
+  /* 'reg' argument is unused */
+  ret = ctx->read_reg(ctx->handle, 0, (uint8_t *)&frame, 1);
+  if (ret < 0)
+  {
+    return -1;
+  }
+
+  ret = asm6g300b_dec_miso_frame(frame, &d, &s);
+  if (ret < 0)
+  {
+    return -1;
+  }
+
+  data[0] = d & 0xff;
+  data[1] = (d >> 8) & 0xff;
+
+  return ret;
+}
+
+/*
+ * Write frame to SafeSPI bus
+ */
+static int32_t asm6g300b_send_req_frame(const stmdev_ctx_t *ctx, uint8_t rw, uint8_t reg,
+                                        uint16_t data)
+{
+  uint32_t fr;
+
+  if (ctx == NULL)
+  {
+    return -1;
+  }
+
+  fr = asm6g300b_gen_mosi_frame(reg, rw, data);
+
+  /* send frame. Pls note that 'reg' is already incapsulated in frame */
+  return ctx->write_reg(ctx->handle, 0, (uint8_t *)&fr, 1);
+}
+
+/**
+  * @brief  Read raw data from reg channel
+  *
+  * @param  ctx   read / write interface definitions(ptr)
+  * @param  reg   register to read
+  * @param  raw   buffer to retrieve data(ptr)
+  * @retval       interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t __weak asm6g300b_read_reg(const stmdev_ctx_t *ctx, uint8_t reg, uint16_t *raw)
+{
+  int32_t ret;
+  uint16_t tmp = 0;
+  uint8_t sa;
+
+  ret = asm6g300b_send_req_frame(ctx, 0, reg, tmp);
+  if (ret == -1)
+  {
+    return -1;
+  }
+
+  return asm6g300b_recv_rsp_frame(ctx, &sa, (uint8_t *)raw);
+}
+
+/**
+  * @brief  Write raw data to reg channel
+  *
+  * @param  ctx   read / write interface definitions(ptr)
+  * @param  reg   register to be written
+  * @param  raw   data to be written
+  * @retval       interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t __weak asm6g300b_write_reg(const stmdev_ctx_t *ctx, uint8_t reg, uint16_t raw)
+{
+  return asm6g300b_send_req_frame(ctx, 1, reg, raw);
+}
+
+/**
+  * @}
+  *
+  */
+
+/**
+  * @brief  Set Device Command
+  *
+  * @param  ctx      read / write interface definitions
+  * @param  cmd      Device command to be executed
+  * @retval          interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_set_command(const stmdev_ctx_t *ctx, asm6g300b_commands_t cmd)
+{
+  uint16_t val = (uint16_t)cmd;
+  int32_t ret;
+
+  ret = asm6g300b_send_req_frame(ctx, 1, ASM6G300B_CONFIG02, val);
+
+  return ret;
+}
+
+/**
+  * @brief  Device startup
+  *
+  * @param  ctx   communication interface handler.(ptr)
+  * @retval       interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_startup(const stmdev_ctx_t *ctx)
+{
+  int32_t ret;
+  asm6g300b_config01_t cfg01 = {0};
+  asm6g300b_config04_t cfg04 = {0};
+  asm6g300b_config05_t cfg05 = {0};
+  asm6g300b_config06_t cfg06 = {0};
+  uint16_t *cfgp = (uint16_t *)&cfg01;
+  uint16_t tmp;
+  asm6g300b_priv_t *cfg_priv;
+
+  if (ctx == NULL || ctx->mdelay == NULL || ctx->priv_data == NULL)
+  {
+    return -1;
+  }
+  ctx->mdelay(25);
+
+  cfg_priv = ctx->priv_data;
+
+  /* Perform hard reset */
+  ret = asm6g300b_set_command(ctx, ASM6G300B_CMD_HARD_RESET);
+  ctx->mdelay(20);
+
+  /* Configure filters and SDO strength */
+  cfg01.sdo_drv = cfg_priv->sdo_drv;
+  cfg01.iir_bw_sel_ax = cfg_priv->iir_bw_sel_ax;
+  cfg01.iir_bw_sel_ay = cfg_priv->iir_bw_sel_ay;
+  cfg01.iir_bw_sel_az = cfg_priv->iir_bw_sel_az;
+  cfg01.iir_bw_sel_rx = cfg_priv->iir_bw_sel_rx;
+  cfg01.iir_bw_sel_rz = cfg_priv->iir_bw_sel_rz;
+  cfgp = (uint16_t *)&cfg01;
+  ret = asm6g300b_send_req_frame(ctx, 1, ASM6G300B_CONFIG01, *cfgp);
+  if (ret == -1)
+  {
+    return -1;
+  }
+
+  /* Configure debounce times */
+  cfg04.tdebrng_ax = cfg_priv->t_debounce_ax;
+  cfg04.tdebrng_ay = cfg_priv->t_debounce_ay;
+  cfg04.tdebrng_az = cfg_priv->t_debounce_az;
+  cfg04.tdebrng_rx = cfg_priv->t_debounce_rx;
+  cfg04.tdebrng_rz = cfg_priv->t_debounce_rz;
+  cfgp = (uint16_t *)&cfg04;
+  ret = asm6g300b_send_req_frame(ctx, 1, ASM6G300B_CONFIG04, *cfgp);
+  if (ret == -1)
+  {
+    return -1;
+  }
+
+  /* Configure Z clamp */
+  cfg05.zclampconfig = cfg_priv->z_clamp;
+  cfgp = (uint16_t *)&cfg05;
+  ret = asm6g300b_send_req_frame(ctx, 1, ASM6G300B_CONFIG05, *cfgp);
+  if (ret == -1)
+  {
+    return -1;
+  }
+
+  /* Configure leftovers */
+  cfg06.iir_bw_sel_ry = cfg_priv->iir_bw_sel_ry;
+  cfg06.tdebrng_ry = cfg_priv->t_debounce_ry;
+  cfgp = (uint16_t *)&cfg06;
+  ret = asm6g300b_send_req_frame(ctx, 1, ASM6G300B_CONFIG06, *cfgp);
+  if (ret == -1)
+  {
+    return -1;
+  }
+
+  ctx->mdelay(10);
+
+  /* wake-up sensor */
+  ret = asm6g300b_set_command(ctx, ASM6G300B_CMD_WAKE_UP);
+  if (ret == -1)
+  {
+    return -1;
+  }
+  ctx->mdelay(350);
+
+  /* read all status register to clear the events */
+  asm6g300b_read_reg(ctx, ASM6G300B_SSUMOK, &tmp);
+  asm6g300b_read_reg(ctx, ASM6G300B_SSUMRNG, &tmp);
+  asm6g300b_read_reg(ctx, ASM6G300B_STATARSX, &tmp);
+  asm6g300b_read_reg(ctx, ASM6G300B_STATARSY, &tmp);
+  asm6g300b_read_reg(ctx, ASM6G300B_STATARSZ, &tmp);
+  asm6g300b_read_reg(ctx, ASM6G300B_STATACCX, &tmp);
+  asm6g300b_read_reg(ctx, ASM6G300B_STATACCY, &tmp);
+  asm6g300b_read_reg(ctx, ASM6G300B_STATACCZ, &tmp);
+  asm6g300b_read_reg(ctx, ASM6G300B_STATCOM1, &tmp);
+  asm6g300b_read_reg(ctx, ASM6G300B_STATCOM2, &tmp);
+  ctx->mdelay(10);
+
+  if (cfg_priv->disable_auto_self_test == 0)
+  {
+    /* Invoke Acc/Ars Auto self-test */
+    ret = asm6g300b_set_command(ctx, ASM6G300B_CMD_ALL_AUTO_SELF_TEST);
+    if (ret == -1)
+    {
+      return -1;
+    }
+
+    /* check self-test result */
+    uint32_t st_status = 0, retry = 0;
+    do
+    {
+      ctx->mdelay(50);
+      asm6g300b_st_status_get(ctx, &st_status);
+    } while ((st_status & ASM6G300B_ST_STATUS_MASK) != ASM6G300B_ST_STATUS_OK && retry++ < 5);
+
+    if (retry >= 5)
+    {
+      return -1;
+    }
+  }
+
+  /* Put sensor in Normal mode */
+  ret = asm6g300b_set_command(ctx, ASM6G300B_CMD_EOI);
+  if (ret == -1)
+  {
+    return -1;
+  }
+
+  return ret;
+}
+
+/**
+  * @brief  Check  communication on SPI bus
+  *
+  * @param  ctx   communication interface handler.(ptr)
+  * @retval       interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_check_spi_communication(const stmdev_ctx_t *ctx)
+{
+  int32_t ret;
+  uint16_t val1, val2 = 0;
+  uint8_t reg = 0;
+
+  val1 = 0x1234;
+
+  ret = asm6g300b_send_req_frame(ctx, 1, ASM6G300B_TEST_REG, val1);
+  if (ret == -1)
+  {
+    return -1;
+  }
+
+  val2 = 0;
+  ret = asm6g300b_recv_rsp_frame(ctx, &reg, (uint8_t *)&val2);
+  if (ret == -1)
+  {
+    return -1;
+  }
+
+  if (val1 != val2)
+  {
+    return -1;
+  }
+
+  return ret;
+}
+
+/**
+  * @brief  Get Device Summary Status register
+  *
+  * @param  ctx    communication interface handler.(ptr)
+  * @param  status Summary status register content .(ptr)
+  * @retval        interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_summary_status_get(const stmdev_ctx_t *ctx, uint16_t *status)
+{
+  return asm6g300b_read_reg(ctx, ASM6G300B_SSUMOK, status);
+}
+
+/**
+  * @brief  Get Device Summary Signal Range Status register
+  *
+  * @param  ctx    communication interface handler.(ptr)
+  * @param  status Summary signal range status register content .(ptr)
+  * @retval        interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_summary_sig_range_status_get(const stmdev_ctx_t *ctx, uint16_t *status)
+{
+  return asm6g300b_read_reg(ctx, ASM6G300B_SSUMRNG, status);
+}
+
+/**
+  * @brief  Get ARS Status registers
+  *
+  * @param  ctx    communication interface handler.(ptr)
+  * @param  status ARS status registers content .(ptr to 3 uint16_t)
+  * @retval        interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_ars_status_get(const stmdev_ctx_t *ctx, uint16_t *status)
+{
+  int32_t ret;
+
+  ret = asm6g300b_read_reg(ctx, ASM6G300B_STATARSX, &status[0]);
+  ret += asm6g300b_read_reg(ctx, ASM6G300B_STATARSY, &status[1]);
+  ret += asm6g300b_read_reg(ctx, ASM6G300B_STATARSZ, &status[2]);
+
+  return ret;
+}
+
+/**
+  * @brief  Get ACC Status registers
+  *
+  * @param  ctx    communication interface handler.(ptr)
+  * @param  status ACC status registers content .(ptr to 3 uint16_t)
+  * @retval        interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_acc_status_get(const stmdev_ctx_t *ctx, uint16_t *status)
+{
+  int32_t ret;
+
+  ret = asm6g300b_read_reg(ctx, ASM6G300B_STATACCX, &status[0]);
+  ret += asm6g300b_read_reg(ctx, ASM6G300B_STATACCY, &status[1]);
+  ret += asm6g300b_read_reg(ctx, ASM6G300B_STATACCZ, &status[2]);
+
+  return ret;
+}
+
+/**
+  * @brief  Get Common Mode Status register
+  *         It chains StatCOM1 and StatCOM2 together
+  *
+  * @param  ctx    communication interface handler.(ptr)
+  * @param  status Summary status register content .(ptr)
+  * @retval        interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_com_status_get(const stmdev_ctx_t *ctx, uint32_t *status)
+{
+  int32_t ret;
+  uint16_t temp1, temp2;
+
+  ret = asm6g300b_read_reg(ctx, ASM6G300B_STATCOM1, &temp1);
+  ret += asm6g300b_read_reg(ctx, ASM6G300B_STATCOM2, &temp2);
+
+  *status = (temp2 << 16) | temp1;
+
+  return ret;
+}
+
+/**
+  * @brief  Get Self-Test Status registers
+  *         It chains ST_STATUS_ARS and ST_STATUS_ACC together
+  *
+  * @param  ctx    communication interface handler.(ptr)
+  * @param  status Summary status register content .(ptr)
+  * @retval        interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_st_status_get(const stmdev_ctx_t *ctx, uint32_t *status)
+{
+  int32_t ret;
+  uint16_t temp1, temp2;
+
+  ret = asm6g300b_read_reg(ctx, ASM6G300B_ST_STATUS_ARS, &temp1);
+  ret += asm6g300b_read_reg(ctx, ASM6G300B_ST_STATUS_ACC, &temp2);
+
+  *status = (temp2 << 16) | temp1;
+
+  return ret;
+}
+
+int32_t asm6g300b_from_ars_lsb_to_mdps(int16_t lsb)
+{
+  return 20 * lsb; /* unit is mdps */
+}
+
+int32_t asm6g300b_from_acc_lsb_to_mms2(int16_t lsb)
+{
+  return 5 * lsb; /* unit is mm/s^2 */
+}
+
+int32_t asm6g300b_from_temp_lsb_to_celsius(int16_t lsb)
+{
+  return (25000 + 40 * lsb); /* unit is milliCelsius degrees */
+}
+
+/**
+  * @brief  Angular rate data.[get]
+  *
+  * @param  ctx   communication interface handler.(ptr)
+  * @param  raw   lsb data retrived from the sensor.(ptr)
+  * @retval       interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_ars_data_get(const stmdev_ctx_t *ctx, int16_t *raw)
+{
+  int32_t ret;
+
+  ret = asm6g300b_read_reg(ctx, ASM6G300B_ARSX, (uint16_t *)&raw[0]);
+  ret += asm6g300b_read_reg(ctx, ASM6G300B_ARSY, (uint16_t *)&raw[1]);
+  ret += asm6g300b_read_reg(ctx, ASM6G300B_ARSZ, (uint16_t *)&raw[2]);
+
+  return ret;
+}
+
+/**
+  * @brief  Low-g Accelerometer data.[get]
+  *
+  * @param  ctx   communication interface handler.(ptr)
+  * @param  raw   lsb data retrived from the sensor.(ptr)
+  * @retval       interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_acc_data_get(const stmdev_ctx_t *ctx, int16_t *raw)
+{
+  int32_t ret;
+
+  ret = asm6g300b_read_reg(ctx, ASM6G300B_ACCX, (uint16_t *)&raw[0]);
+  ret += asm6g300b_read_reg(ctx, ASM6G300B_ACCY, (uint16_t *)&raw[1]);
+  ret += asm6g300b_read_reg(ctx, ASM6G300B_ACCZ, (uint16_t *)&raw[2]);
+
+  return ret;
+}
+
+/**
+  * @brief  Temperature data.[get]
+  *
+  * @param  ctx   communication interface handler.(ptr)
+  * @param  raw   lsb data retrived from the sensor.(ptr)
+  * @retval       interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_temp_data_get(const stmdev_ctx_t *ctx, int16_t *raw)
+{
+  return asm6g300b_read_reg(ctx, ASM6G300B_TEMP, (uint16_t *)raw);
+}
+
+/**
+  * @brief  Get Device serial number.[get]
+  *
+  * @param  ctx   communication interface handler.(ptr)
+  * @param  s_num Serial number.(ptr)
+  * @retval       interface status (MANDATORY: return 0 -> no Error)
+  *
+  */
+int32_t asm6g300b_getSerialNum(const stmdev_ctx_t *ctx, uint32_t *s_num)
+{
+  uint16_t tmp0 = 0, tmp1 = 0;
+  int32_t ret;
+
+  ret = asm6g300b_read_reg(ctx, ASM6G300B_ASIC_ID, &tmp0);
+  ret += asm6g300b_read_reg(ctx, ASM6G300B_ID_TRACKING2, &tmp1);
+  if (ret == -1)
+  {
+    return -1;
+  }
+
+  *s_num = ((tmp0 & 0xf800) << 5) + tmp1;
+
+  return ret;
+}
+
+/**
+  * @}
+  *
+  */
---
+++ asm6g300b_STdC/examples/asm6g300b_read.c
@@ -0,0 +1,395 @@
+/*
+ ******************************************************************************
+ * @file    read.c
+ * @author  Sensors Software Solution Team
+ * @brief   This file show the simplest way to get data from sensor.
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+
+/*
+ * This example was developed using the following STMicroelectronics
+ * evaluation boards:
+ *
+ * - STEVAL_MKI109V3 + STEVAL-MKI208V1K
+ * - DISCOVERY_SPC584B + STEVAL-MKI208V1K
+ *
+ * and STM32CubeMX tool with STM32CubeF4 MCU Package
+ *
+ * Used interfaces:
+ *
+ * STEVAL_MKI109V3    - Host side:   USB (Virtual COM)
+ *                    - Sensor side: SPI(Default) / I2C(supported)
+ *
+ * DISCOVERY_SPC584B  - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I2C(Default) / SPI(supported)
+ *
+ *
+ * If you need to run this example on a different hardware platform a
+ * modification of the functions: `platform_write`, `platform_read`,
+ * `tx_com` and 'platform_init' is required.
+ *
+ */
+
+/* STMicroelectronics evaluation boards definition
+ *
+ * Please uncomment ONLY the evaluation boards in use.
+ * If a different hardware is used please comment all
+ * following target board and redefine yours.
+ */
+
+//#define STEVAL_MKI109V3  /* little endian */
+//#define SPC584B_DIS      /* big endian */
+
+/* ATTENTION: By default the driver is little endian. If you need switch
+ *            to big endian please see "Endianness definitions" in the
+ *            header file of the driver (_reg.h).
+ */
+
+#if defined(STEVAL_MKI109V3)
+/* MKI109V3: Define communication interface */
+#define SENSOR_BUS hspi2
+/* MKI109V3: Vdd and Vddio power supply values */
+#define PWM_3V3 915
+
+#elif defined(SPC584B_DIS)
+/* DISCOVERY_SPC584B: Define communication interface */
+#define SENSOR_BUS NULL
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <string.h>
+#include <stdio.h>
+#include "asm6g300b_reg.h"
+
+#if defined(NUCLEO_F411RE)
+#include "stm32f4xx_hal.h"
+#include "usart.h"
+#include "gpio.h"
+#include "i2c.h"
+
+#elif defined(STEVAL_MKI109V3)
+#include "stm32f4xx_hal.h"
+#include "usbd_cdc_if.h"
+#include "gpio.h"
+#include "spi.h"
+#include "tim.h"
+
+#elif defined(SPC584B_DIS)
+#include "components.h"
+#endif
+
+/* Private macro -------------------------------------------------------------*/
+#define    BOOT_TIME        25 //ms
+
+/* Private variables ---------------------------------------------------------*/
+static uint8_t tx_buffer[1000];
+
+/* Extern variables ----------------------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+
+/*
+ *   WARNING:
+ *   Functions declare in this section are defined at the end of this file
+ *   and are strictly related to the hardware platform used.
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len);
+static void tx_com( uint8_t *tx_buffer, uint16_t len );
+static void platform_delay(uint32_t ms);
+static void platform_init(void);
+
+/* Main Example --------------------------------------------------------------*/
+static void check_device_status(stmdev_ctx_t *ctx)
+{
+    uint16_t status;
+
+    asm6g300b_summary_status_get(ctx, &status);
+    sprintf((char *)tx_buffer, "Summary status %04x\r\n", status);
+    //tx_com(tx_buffer, strlen((char const *)tx_buffer));
+
+    asm6g300b_summary_sig_range_status_get(ctx, &status);
+    sprintf((char *)tx_buffer, "Summary signal range status %04x\r\n", status);
+    //tx_com(tx_buffer, strlen((char const *)tx_buffer));
+
+#if 0
+    if ((status & 0x400) == 0)
+    {
+      /* S_OK_C is zero */
+      uint32_t com_status;
+
+      asm6g300b_com_status_get(ctx, &com_status);
+      sprintf((char *)tx_buffer, "COMMON status %08x\r\n", com_status);
+      tx_com(tx_buffer, strlen((char const *)tx_buffer));
+    }
+#endif
+}
+
+static uint32_t num_samples = 0;
+
+static void asm6g300b_read_all_sensors_task(stmdev_ctx_t *ctx)
+{
+    int16_t raw[3];
+    int32_t accel[3], gyro[3], temp;
+    uint8_t *tx_p;
+    uint16_t num;
+
+    check_device_status(ctx);
+    tx_p = tx_buffer;
+
+    num = sprintf((char *)tx_p, "NUM %d\r\n", num_samples);
+    tx_p += num;
+
+    /* Get low-g accel data */
+    asm6g300b_acc_data_get(ctx, raw);
+
+    accel[0] = asm6g300b_from_acc_lsb_to_mms2(raw[0]);
+    accel[1] = asm6g300b_from_acc_lsb_to_mms2(raw[1]);
+    accel[2] = asm6g300b_from_acc_lsb_to_mms2(raw[2]);
+
+    num = sprintf((char *)tx_p, "LOW-G %d (mm/s^2) %d (mm/s^2) %d (mm/s^2)\r\n",
+                                accel[0], accel[1], accel[2]);
+    tx_p += num;
+
+    /* Get angular rate data */
+    asm6g300b_ars_data_get(ctx, raw);
+
+    gyro[0] = asm6g300b_from_ars_lsb_to_mdps(raw[0]);
+    gyro[1] = asm6g300b_from_ars_lsb_to_mdps(raw[1]);
+    gyro[2] = asm6g300b_from_ars_lsb_to_mdps(raw[2]);
+
+    num = sprintf((char *)tx_p, "GYRO %d (mdps) %d (mdps) %d (mdps)\r\n",
+                                gyro[0], gyro[1], gyro[2]);
+    tx_p += num;
+
+    /* Get TEMPERATURE data */
+    asm6g300b_temp_data_get(ctx, &raw[0]);
+    temp = asm6g300b_from_temp_lsb_to_celsius(raw[0]);
+
+    sprintf((char *)tx_p, "TEMP %d (milli Celsius)\r\n\r\n", temp);
+}
+
+static asm6g300b_priv_t asm6g300b_config =
+  {
+    .iir_bw_sel_ax = ASM6G300B_IIR_FILTER_60HZ,
+    .iir_bw_sel_ay = ASM6G300B_IIR_FILTER_60HZ,
+    .iir_bw_sel_az = ASM6G300B_IIR_FILTER_60HZ,
+    .iir_bw_sel_rx = ASM6G300B_IIR_FILTER_60HZ,
+    .iir_bw_sel_ry = ASM6G300B_IIR_FILTER_60HZ,
+    .iir_bw_sel_rz = ASM6G300B_IIR_FILTER_60HZ,
+
+    .t_debounce_ax = ASM6G300B_DEBOUNCE_TIME_0MS,
+    .t_debounce_ay = ASM6G300B_DEBOUNCE_TIME_0MS,
+    .t_debounce_az = ASM6G300B_DEBOUNCE_TIME_0MS,
+    .t_debounce_rx = ASM6G300B_DEBOUNCE_TIME_0MS,
+    .t_debounce_ry = ASM6G300B_DEBOUNCE_TIME_0MS,
+    .t_debounce_rz = ASM6G300B_DEBOUNCE_TIME_0MS,
+
+    .z_clamp = ASM6G300B_NVM_CLIPPING_DEFAULT,
+
+    .sdo_drv = 0,
+    .disable_auto_self_test = 0,
+  };
+
+void asm6g300b_read(void)
+{
+  stmdev_ctx_t dev_ctx;
+  int ret;
+
+  /* Initialize mems driver interface */
+  dev_ctx.write_reg = platform_write;
+  dev_ctx.read_reg = platform_read;
+  dev_ctx.mdelay = platform_delay;
+  dev_ctx.handle = &SENSOR_BUS;
+  dev_ctx.priv_data = &asm6g300b_config;
+
+  /* Init test platform */
+  platform_init();
+
+  /* Wait sensor boot time */
+  platform_delay(BOOT_TIME);
+
+  ret = asm6g300b_startup(&dev_ctx);
+  if (ret < 0)
+  {
+    while(1); /* device not started */
+  }
+
+  if (asm6g300b_check_spi_communication(&dev_ctx) < 0)
+  {
+    while(1); /* stop here if SPI communication is not established */
+  }
+
+  uint32_t serial_num;
+  asm6g300b_getSerialNum(&dev_ctx, &serial_num);
+
+  sprintf((char *)tx_buffer, "serial_num %08x\r\n", serial_num);
+  tx_com(tx_buffer, strlen((char const *)tx_buffer));
+
+  uint32_t tick_start = HAL_GetTick();
+
+  while(1)
+  {
+    uint32_t tick_curr;
+
+    tick_curr = HAL_GetTick();
+
+    /* simulate a thread scheduled every 1ms (1kHz) */
+    if ((tick_curr - tick_start) >= 1)
+    {
+      tick_start = tick_curr;
+      asm6g300b_read_all_sensors_task(&dev_ctx);
+
+      /* prints out only every 1000 samples */
+      if ((num_samples++ % 1000) == 999)
+        tx_com(tx_buffer, strlen((char const *)tx_buffer));
+    }
+  }
+}
+
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to write (unused)
+ * @param  bufp      pointer to data to write in register reg
+ * @param  len       number of consecutive register to write
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+  (void)reg; /* 'reg' argument is anused (it's already embedded in bufp) */
+
+#ifdef STEVAL_MKI109V3
+  uint8_t txBuf[4];
+
+  if (handle == &hspi2) {
+#if DRV_BYTE_ORDER == DRV_LITTLE_ENDIAN
+      /* on SPI bus dta is sent big endian */
+      txBuf[0] = bufp[3];
+      txBuf[1] = bufp[2];
+      txBuf[2] = bufp[1];
+      txBuf[3] = bufp[0];
+#endif /* DRV_BYTE_ORDER */
+
+      HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+      uint8_t status = (HAL_SPI_Transmit(handle, txBuf, 4, HAL_MAX_DELAY) == HAL_OK);
+      while(HAL_SPI_GetState(handle) != HAL_SPI_STATE_READY);
+      HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+  }
+
+#elif defined(SPC584B_DIS)
+  /* Add here the SPC5 write SPI interface */
+#endif
+  return 0;
+}
+
+/*
+ * @brief  Read generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to read
+ * @param  bufp      pointer to buffer that store the data read
+ * @param  len       number of consecutive register to read
+ *
+ */
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+  (void)reg; /* 'reg' argument is anused (it's already embedded in bufp) */
+
+#ifdef STEVAL_MKI109V3
+  uint8_t txBuf[4];
+  uint8_t rxBuf[4];
+
+  if (handle == &hspi2) {
+#if DRV_BYTE_ORDER == DRV_LITTLE_ENDIAN
+      /* on SPI bus dta is sent big endian */
+      txBuf[0] = bufp[3];
+      txBuf[1] = bufp[2];
+      txBuf[2] = bufp[1];
+      txBuf[3] = bufp[0];
+#endif /* DRV_BYTE_ORDER */
+
+      HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+      HAL_SPI_TransmitReceive(handle, txBuf,rxBuf, 4, HAL_MAX_DELAY);
+      while(HAL_SPI_GetState(handle) != HAL_SPI_STATE_READY);
+      HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+
+#if DRV_BYTE_ORDER == DRV_LITTLE_ENDIAN
+      /* on SPI bus dta is sent big endian */
+      bufp[0] = rxBuf[3];
+      bufp[1] = rxBuf[2];
+      bufp[2] = rxBuf[1];
+      bufp[3] = rxBuf[0];
+#endif /* DRV_BYTE_ORDER */
+ }
+
+#elif defined(SPC584B_DIS)
+  /* Add here the SPC5 read SPI interface */
+#endif
+  return 0;
+}
+
+/*
+ * @brief  Send buffer to console (platform dependent)
+ *
+ * @param  tx_buffer     buffer to transmit
+ * @param  len           number of byte to send
+ *
+ */
+static void tx_com(uint8_t *tx_buffer, uint16_t len)
+{
+#ifdef STEVAL_MKI109V3
+  CDC_Transmit_FS(tx_buffer, len);
+#elif defined(SPC584B_DIS)
+  sd_lld_write(&SD2, tx_buffer, len);
+#endif
+}
+
+/*
+ * @brief  platform specific delay (platform dependent)
+ *
+ * @param  ms        delay in ms
+ *
+ */
+static void platform_delay(uint32_t ms)
+{
+#if defined(NUCLEO_F411RE) | defined(STEVAL_MKI109V3)
+  HAL_Delay(ms);
+#elif defined(SPC584B_DIS)
+  osalThreadDelayMilliseconds(ms);
+#endif
+}
+
+/*
+ * @brief  platform specific initialization (platform dependent)
+ */
+static void platform_init(void)
+{
+#if defined(STEVAL_MKI109V3)
+  TIM3->CCR1 = PWM_3V3;
+  TIM3->CCR2 = PWM_3V3;
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_Delay(1000);
+#endif
+}
