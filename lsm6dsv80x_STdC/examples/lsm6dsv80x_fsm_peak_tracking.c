f32a9d8e627b2783947180e36f883ae6503e6700
Armando Visconti <armando.visconti@st.com>
2026-05-13 16:08:19 +0200

    lsm6dsv80x: add FSM peak tracking example
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

---
+++ lsm6dsv80x_STdC/examples/lsm6dsv80x_fsm_peak_tracking.c
@@ -0,0 +1,392 @@
+/*
+ ******************************************************************************
+ * @file    fsm_peak_training.c
+ * @author  Sensors Software Solution Team
+ * @brief   This file shows how to generate and handle a Free Fall event.
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+
+/*
+ * This example was developed using the following STMicroelectronics
+ * evaluation boards:
+ *
+ * - STEVAL_MKI109V3 + STEVAL-MKI247A
+ * - STEVAL_MKI109D  + STEVAL-MKI247A
+ * - NUCLEO_F401RE + X_NUCLEO_IKS01A3
+ * - DISCOVERY_SPC584B + STEVAL-MKI247A
+ *
+ * Used interfaces:
+ *
+ * STEVAL_MKI109D     - Host side:   USB (Virtual COM)
+ *                    - Sensor side: SPI(Default) / I2C(supported)
+ *
+ * STEVAL_MKI109V3    - Host side:   USB (Virtual COM)
+ *                    - Sensor side: SPI(Default) / I2C(supported)
+ *
+ * NUCLEO_STM32F401RE - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I2C(Default) / SPI(supported)
+ *
+ * DISCOVERY_SPC584B  - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I2C(Default) / SPI(supported)
+ *
+ * NUCLEO_STM32H503RG - Host side: UART(COM) to USB bridge
+ *                    - Sensor side: I3C(Default)
+ *
+ * If you need to run this example on a different hardware platform a
+ * modification of the functions: `platform_write`, `platform_read`,
+ * `tx_com` and 'platform_init' is required.
+ *
+ */
+
+/* STMicroelectronics evaluation boards definition
+ *
+ * Please uncomment ONLY the evaluation boards in use.
+ * If a different hardware is used please comment all
+ * following target board and redefine yours.
+ */
+
+//#define STEVAL_MKI109V3  /* little endian */
+//#define NUCLEO_F401RE    /* little endian */
+//#define SPC584B_DIS      /* big endian */
+
+/* ATTENTION: By default the driver is little endian. If you need switch
+ *            to big endian please see "Endianness definitions" in the
+ *            header file of the driver (_reg.h).
+ */
+
+#if defined(STEVAL_MKI109D)
+/* MKI109D: Define communication interface */
+#define SENSOR_BUS hspi1
+
+/* MKI109D: Vdd and Vddio power supply values */
+#define LSM6DSV16X_VDD 1.8f
+#define LSM6DSV16X_VDDIO 1.8f
+
+#elif defined(STEVAL_MKI109V3)
+/* MKI109V3: Define communication interface */
+#define SENSOR_BUS hspi2
+/* MKI109V3: Vdd and Vddio power supply values */
+#define PWM_3V3 915
+
+#elif defined(NUCLEO_F401RE)
+/* NUCLEO_F401RE: Define communication interface */
+#define SENSOR_BUS hi2c1
+
+#elif defined(SPC584B_DIS)
+/* DISCOVERY_SPC584B: Define communication interface */
+#define SENSOR_BUS I2CD1
+
+#elif defined(NUCLEO_H503RB)
+/* NUCLEO_H503RB: Define communication interface */
+#define SENSOR_BUS hi3c1
+
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <string.h>
+#include <stdio.h>
+#include "lsm6dsv80x_peak_tracking.h"
+#include "lsm6dsv80x_reg.h"
+
+#if defined(NUCLEO_F401RE)
+#include "stm32f4xx_hal.h"
+#include "usart.h"
+#include "gpio.h"
+#include "i2c.h"
+
+#elif defined(STEVAL_MKI109D)
+#include "board.h"
+#include "usbd_cdc_if.h"
+
+#elif defined(STEVAL_MKI109V3)
+#include "stm32f4xx_hal.h"
+#include "usbd_cdc_if.h"
+#include "gpio.h"
+#include "spi.h"
+#include "tim.h"
+
+#elif defined(SPC584B_DIS)
+#include "components.h"
+
+#elif defined(NUCLEO_H503RB)
+#include "usart.h"
+#include "i3c.h"
+#include "i3c_api.h"
+#include <stdio.h>
+
+static uint8_t i3c_dyn_addr = 0x0A;
+#endif
+
+/* Private macro -------------------------------------------------------------*/
+#define    BOOT_TIME            10 //ms
+
+/* Private variables ---------------------------------------------------------*/
+static uint8_t whoamI;
+static uint8_t tx_buffer[1000];
+
+/* Extern variables ----------------------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+/*
+ *   WARNING:
+ *   Functions declare in this section are defined at the end of this file
+ *   and are strictly related to the hardware platform used.
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len);
+static void tx_com( uint8_t *tx_buffer, uint16_t len );
+static void platform_delay(uint32_t ms);
+static void platform_init(void *handle);
+
+static stmdev_ctx_t dev_ctx;
+static volatile uint8_t peak_catched;
+
+void lsm6dsv80x_fsm_peak_training_handler(void)
+{
+  peak_catched = 1;
+}
+
+/* Main Example --------------------------------------------------------------*/
+void lsm6dsv80x_fsm_peak_training(void)
+{
+  float_t acceleration_mg[3];
+  uint32_t i;
+
+  /* Initialize mems driver interface */
+  dev_ctx.write_reg = platform_write;
+  dev_ctx.read_reg = platform_read;
+  dev_ctx.mdelay = platform_delay;
+  dev_ctx.handle = &SENSOR_BUS;
+
+  /* Init test platform */
+  platform_init(dev_ctx.handle);
+
+  /* Wait sensor boot time */
+  platform_delay(BOOT_TIME);
+
+  /* Check device ID */
+  lsm6dsv80x_device_id_get(&dev_ctx, &whoamI);
+
+  if (whoamI != LSM6DSV80X_ID)
+    while (1);
+
+  /* Perform device power-on-reset */
+  lsm6dsv80x_sw_por(&dev_ctx);
+
+#if defined(NUCLEO_H503RB)
+  /* if I3C is used then INT pin must be explicitly enabled */
+  lsm6dsv80x_i3c_int_en_set(&dev_ctx, 1);
+#endif
+
+  /* Start FSM configuration */
+  for ( i = 0; i < (sizeof(lsm6dsv80x_peak_tracking_conf_0) /
+                    sizeof(struct mems_conf_op) ); i++ ) {
+    switch(lsm6dsv80x_peak_tracking_conf_0[i].type) {
+    case MEMS_CONF_OP_TYPE_DELAY:
+      platform_delay(lsm6dsv80x_peak_tracking_conf_0[i].data);
+      break;
+    case MEMS_CONF_OP_TYPE_WRITE:
+      lsm6dsv80x_write_reg(&dev_ctx, lsm6dsv80x_peak_tracking_conf_0[i].address,
+                           (uint8_t *)&lsm6dsv80x_peak_tracking_conf_0[i].data, 1);
+      break;
+    }
+  }
+
+  /* wait forever (FF event handle in irq handler) */
+  while (1) {
+    if (peak_catched) {
+      uint8_t fsm_status, buff[2];
+      lsm6dsv80x_fifo_out_raw_t f_data;
+      int16_t *datax;
+      int16_t *datay;
+      int16_t *dataz;
+
+      peak_catched = 0;
+
+      lsm6dsv80x_fsm_out_t fsm_outs;
+      lsm6dsv80x_read_reg(&dev_ctx, LSM6DSV80X_FSM_STATUS_MAINPAGE, &fsm_status, 1);
+      lsm6dsv80x_fsm_out_get(&dev_ctx, &fsm_outs);
+      if ((fsm_status == 0x1) && (fsm_outs.fsm_outs1 == 0))
+      {
+        /* Read FIFO sensor value */
+        lsm6dsv80x_fifo_out_raw_get(&dev_ctx, &f_data);
+        datax = (int16_t *)&f_data.data[0];
+        datay = (int16_t *)&f_data.data[2];
+        dataz = (int16_t *)&f_data.data[4];
+
+        switch (f_data.tag) {
+        case LSM6DSV80X_HG_XL_PEAK_TAG:
+          acceleration_mg[0] = lsm6dsv80x_from_fs80_to_mg(*datax)/1000.0f;
+          acceleration_mg[1] = lsm6dsv80x_from_fs80_to_mg(*datay)/1000.0f;
+          acceleration_mg[2] = lsm6dsv80x_from_fs80_to_mg(*dataz)/1000.0f;
+          snprintf((char *)tx_buffer, sizeof(tx_buffer), "hg PEAK:%4.2f g\t%4.2f g\t%4.2f g\r\n",
+                  acceleration_mg[0], acceleration_mg[1], acceleration_mg[2]);
+          tx_com(tx_buffer, strlen((char const *)tx_buffer));
+          break;
+
+        default:
+          snprintf((char *)tx_buffer, sizeof(tx_buffer), "[%02x] UNHANDLED TAG \r\n", f_data.tag);
+          tx_com(tx_buffer, strlen((char const *)tx_buffer));
+          break;
+        }
+      }
+    }
+  }
+}
+
+/*
+ * @brief  Write generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to write
+ * @param  bufp      pointer to data to write in register reg
+ * @param  len       number of consecutive register to write
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+#if defined(NUCLEO_F401RE)
+  HAL_I2C_Mem_Write(handle, LSM6DSV80X_I2C_ADD_L, reg,
+                    I2C_MEMADD_SIZE_8BIT, (uint8_t*) bufp, len, 1000);
+#elif defined(STEVAL_MKI109D)
+  HAL_GPIO_WritePin(SPI1_NSS_GPIO_Port, SPI1_NSS_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, HAL_MAX_DELAY);
+  HAL_SPI_Transmit(handle, (uint8_t*) bufp, len, HAL_MAX_DELAY);
+  HAL_GPIO_WritePin(SPI1_NSS_GPIO_Port, SPI1_NSS_Pin, GPIO_PIN_SET);
+#elif defined(STEVAL_MKI109V3)
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Transmit(handle, (uint8_t*) bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+#elif defined(SPC584B_DIS)
+  i2c_lld_write(handle,  LSM6DSV80X_I2C_ADD_L & 0xFE, reg, (uint8_t*) bufp, len);
+#elif defined(NUCLEO_H503RB)
+  i3c_write(handle, i3c_dyn_addr, reg, (uint8_t*) bufp, len);
+#endif
+  return 0;
+}
+
+/*
+ * @brief  Read generic device register (platform dependent)
+ *
+ * @param  handle    customizable argument. In this examples is used in
+ *                   order to select the correct sensor bus handler.
+ * @param  reg       register to read
+ * @param  bufp      pointer to buffer that store the data read
+ * @param  len       number of consecutive register to read
+ *
+ */
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+#if defined(NUCLEO_F401RE)
+  HAL_I2C_Mem_Read(handle, LSM6DSV80X_I2C_ADD_L, reg,
+                   I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+#elif defined(STEVAL_MKI109D)
+  reg |= 0x80;
+  HAL_GPIO_WritePin(SPI1_NSS_GPIO_Port, SPI1_NSS_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, HAL_MAX_DELAY);
+  HAL_SPI_Receive(handle, bufp, len, HAL_MAX_DELAY);
+  HAL_GPIO_WritePin(SPI1_NSS_GPIO_Port, SPI1_NSS_Pin, GPIO_PIN_SET);
+#elif defined(STEVAL_MKI109V3)
+  reg |= 0x80;
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_RESET);
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Receive(handle, bufp, len, 1000);
+  HAL_GPIO_WritePin(CS_up_GPIO_Port, CS_up_Pin, GPIO_PIN_SET);
+#elif defined(SPC584B_DIS)
+  i2c_lld_read(handle, LSM6DSV80X_I2C_ADD_L & 0xFE, reg, bufp, len);
+#elif defined(NUCLEO_H503RB)
+  i3c_read(handle, i3c_dyn_addr, reg, bufp, len);
+#endif
+  return 0;
+}
+
+/*
+ * @brief  Send buffer to console (platform dependent)
+ *
+ * @param  tx_buffer     buffer to transmit
+ * @param  len           number of byte to send
+ *
+ */
+static void tx_com(uint8_t *tx_buffer, uint16_t len)
+{
+#if defined(NUCLEO_F401RE)
+  HAL_UART_Transmit(&huart2, tx_buffer, len, 1000);
+#elif defined(STEVAL_MKI109D)
+  CDC_Transmit_FS(tx_buffer, len);
+#elif defined(STEVAL_MKI109V3)
+  CDC_Transmit_FS(tx_buffer, len);
+#elif defined(SPC584B_DIS)
+  sd_lld_write(&SD2, tx_buffer, len);
+#elif defined(NUCLEO_H503RB)
+  HAL_UART_Transmit(&huart3, tx_buffer, len, 1000);
+#endif
+}
+
+/*
+ * @brief  platform specific delay (platform dependent)
+ *
+ * @param  ms        delay in ms
+ *
+ */
+static void platform_delay(uint32_t ms)
+{
+#if defined(NUCLEO_F401RE) || defined(STEVAL_MKI109V3) || defined(NUCLEO_H503RB)
+  HAL_Delay(ms);
+#elif defined(STEVAL_MKI109D)
+  delay(ms);
+#elif defined(SPC584B_DIS)
+  osalThreadDelayMilliseconds(ms);
+#endif
+}
+
+/*
+ * @brief  platform specific initialization (platform dependent)
+ */
+static void platform_init(void *handle)
+{
+#if defined(STEVAL_MKI109D)
+  struct spi_conf spi_conf;
+
+  /* init SPI bus communication */
+  spi_conf.wire = WIRE_4;
+  spi_init(&spi_conf);
+
+  /* set VDD/VDDIO on DIL24 */
+  set_vdd(LSM6DSV16X_VDD);
+  set_vddio(LSM6DSV16X_VDDIO);
+  delay(100);
+
+#elif defined(STEVAL_MKI109V3)
+  TIM3->CCR1 = PWM_3V3;
+  TIM3->CCR2 = PWM_3V3;
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+  HAL_Delay(1000);
+
+#elif defined(NUCLEO_H503RB)
+  i3c_set_bus_frequency(handle, 1000000);
+  i3c_rstdaa(handle);
+  i3c_setdasa(handle, LSM6DSV80X_I2C_ADD_L, &i3c_dyn_addr, 1);
+  i3c_set_bus_frequency(handle, 12500000);
+
+#endif
+}
